21425e3ff45099797d2b442c24e0615df783c3e3
Raheem Kawojue <kawojue08@gmail.com>
2023-05-17 17:15:27 +0100

    [add] next app model

---
+++ server/models/NextApp.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose'
+
+const NextAppModel: Schema = new Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "AdvancePt",
+        sparse: true,
+        unique: true,
+        required: true,
+    },
+    next_appointment: String
+})
+
+const NextApp = mongoose.model('AdvancePt', NextAppModel) || NextAppModel
+export default NextApp
