b150edd5a060a851c0ec7c0d4594900be55ad0b7
Raheem Kawojue <kawojue08@gmail.com>
2023-05-17 17:02:36 +0100

    [add] cors options

---
+++ server/configs/corsOption.ts
@@ -0,0 +1,19 @@
+import { CorsOptions } from "cors"
+
+export const allowedOrigins: string[] =  [
+    "http://localhost:2022"
+]
+
+const corsOption: CorsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin as string) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS!"))
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+export default corsOption
