28749f9ae2cab39bf2703f5a9c636d6ba0754c12
Raheem Kawojue <kawojue08@gmail.com>
2023-05-17 05:03:14 +0100

    [edit]

---
+++ server/env.d.ts
@@ -0,0 +1,15 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            EMAIL: string
+            DB_URI: string
+            EMAIL_PSWD: string
+            JWT_SECRET: string
+            CLOUD_NAME: string
+            CLOUD_API_KEY: string
+            CLOUD_API_SECRET: string
+        }
+    }
+}
+
+export {}
