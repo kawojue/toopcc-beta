f8ae4ce6e66ccb536e8ddaa43e8cf0cd4e6595f0
Raheem Kawojue <kawojue08@gmail.com>
2023-05-17 20:43:34 +0100

    [add] roles verif

--- server/middlewares/verifyRoles.ts
+++ server/middlewares/verifyRoles.ts
@@ -1,13 +1,16 @@
 import { ACCESS_DENIED } from '../configs/error'
 import { Response, NextFunction } from 'express'
 
-const verifyRoles = (role: string) => {
+const verifyRoles = (...roles: string[]) => {
     return (req: any, res: Response, next: NextFunction) => {
         if (!req?.roles) return res.status(401).json(ACCESS_DENIED)
+
+        const authRoles: string[] = req.roles
+        const allowedRoles: string[] = [...roles]
+
+        const result: any = allowedRoles.map((role: string) => authRoles.includes(role)).find((value: boolean) => value === true)
+        if (!result) return res.status(401).json(ACCESS_DENIED)
         
-        const roles: string[] = req.roles
-        const validRole: boolean = roles.includes(role)
-        if (!validRole) return res.status(401).json(ACCESS_DENIED)
         next()
     }
 }
