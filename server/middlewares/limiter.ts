26f5bb17915be60bf5ec6814731ffab73bb437b2
Raheem Kawojue <kawojue08@gmail.com>
2023-05-17 20:44:20 +0100

    [add] req limiter

---
+++ server/middlewares/limiter.ts
@@ -0,0 +1,27 @@
+import { ILimiter } from '../type'
+import { Request, Response, NextFunction } from 'express'
+import rateLimit,
+{ RateLimitRequestHandler, Options } from 'express-rate-limit'
+
+export default function limiterFunc({
+    max, timerArr, msg = "Too many requests sent."
+}: ILimiter): RateLimitRequestHandler {
+    const limiter: RateLimitRequestHandler = rateLimit({
+        max, // max attempt
+        windowMs: timerArr[Math.floor(Math.random() * timerArr.length)] * 1000, // try again in
+        message: {
+            message: msg
+        },
+        handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
+            res.status(options.statusCode).json({
+                success: false,
+                action: "warning",
+                msg: options.message?.message
+            })
+        },
+        legacyHeaders: false,
+        standardHeaders: true,
+    })
+
+    return limiter
+}
