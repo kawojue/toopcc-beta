5a6fa968ddda58b3a0626d4470772f855ede556c
Raheem Kawojue <kawojue08@gmail.com>
2023-05-17 17:00:37 +0100

    [setup] server

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+// import utilities
+import cors from 'cors'
+import logger from 'morgan'
+import mongoose from'mongoose'
+import connectDB from './configs/dbConn'
+import corsOption from './configs/corsOption'
+import express, { Application } from 'express'
+import credentials from './middlewares/credentials'
+
+// import routes
+import root from './routes/root'
+
+const app: Application = express()
+const PORT: unknown = process.env.PORT || 2022
+
+connectDB(process.env.DB_URI as string)
+
+// set middlewares
+app.use(credentials)
+app.use(express.json({ limit: '7mb'}))
+app.use(logger('dev'))
+app.use(cors(corsOption))
+app.use(express.urlencoded({ limit: '7mb', extended: true }))
+
+app.use('/', root)
+
+mongoose.connection.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:2022`)
+    })
+})
