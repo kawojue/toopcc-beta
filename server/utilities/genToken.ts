c4b73968c435a399e98ecbcd96a825b76cb5eb00
Raheem Kawojue <kawojue08@gmail.com>
2023-05-18 21:33:30 +0100

    .

---
+++ server/utilities/genToken.ts
@@ -0,0 +1,14 @@
+import jwt, { Secret } from 'jsonwebtoken'
+
+const genToken = (user: string, roles: string[]) => {
+    const token: Secret = jwt.sign(
+        {
+            "userData": { user, roles }
+        },
+        process.env.JWT_SECRET as string,
+        { expiresIn: '90d' }
+    )
+    return token
+}
+
+export default genToken
