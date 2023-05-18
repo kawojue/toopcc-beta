e6d35230ff855b6c54f63a5f0383be7054fa7984
Raheem Kawojue <kawojue08@gmail.com>
2023-05-18 23:01:07 +0100

    [add] modals

--- server/utilities/modal.ts
+++ server/utilities/modal.ts
@@ -8,13 +8,23 @@ const ACCESS_DENIED = { ...ERROR, msg: "Access Denied" }
 
 const INVALID_EMAIL = { ...ERROR, msg: "Invalid Email" }
 
+const INCORRECT_PSWD = { ...ERROR, msg: "Incorrect Password" }
+
+const SMTH_WENT_WRONG = { ...ERROR, msg: "Something went wrong. "}
+
 const FIELDS_REQUIRED = { ...ERROR, msg: "All fields are required" }
 
 const ACCOUNT_NOT_FOUND = { ...ERROR, msg: "Account does not exist." }
 
-const PASSWORD_NOT_MATCH = { ...WARNING, msg: "Password does not match." }
+const PSWD_NOT_MATCH = { ...WARNING, msg: "Password does not match." }
+
+const CURRENT_PSWD = { ...WARNING, msg: "You input your current password." }
+
+const PSWD_CHANGED = { ...SUCCESS, msg: "Password updated successfully." }
 
 export {
     FIELDS_REQUIRED, INVALID_EMAIL, ACCESS_DENIED, SUCCESS,
-    PASSWORD_NOT_MATCH, ACCOUNT_NOT_FOUND, ERROR, WARNING,
+    PSWD_NOT_MATCH, ACCOUNT_NOT_FOUND, ERROR, WARNING,
+    CURRENT_PSWD, INCORRECT_PSWD, PSWD_CHANGED,
+    SMTH_WENT_WRONG,
 }
