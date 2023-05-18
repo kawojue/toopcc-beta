e7966238d9b924df9d02d7eb1229e07996ec2c78
Raheem Kawojue <kawojue08@gmail.com>
2023-05-18 21:37:26 +0100

    ..

---
+++ server/controllers/patients.ts
@@ -0,0 +1,11 @@
+import Patient from '../models/BasicPt'
+import AltPatient from '../models/AdvancePt'
+import { Request, Response } from 'express'
+const asyncHandler = require('express-async-handler')
+
+
+const add = asyncHandler(async (req: Request, res: Response) => {
+
+})
+
+export { add }
