818dbe83c126763664f6ae87a3603e979074e8bd
Raheem Kawojue <kawojue08@gmail.com>
2023-05-18 23:10:34 +0100

    .

--- server/routes/api/patients.ts
+++ server/routes/api/patients.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import jwtVerify from '../../middlewares/jwtVerify'
 
 const patients: Router = Router()
+
+patients.use(jwtVerify)
 
 export default patients
--- server/routes/root.ts
+++ server/routes/root.ts
@@ -1,12 +1,11 @@
 import auth from './auth/auth'
 import patients from './api/patients'
-import jwtVerify from '../middlewares/jwtVerify'
 import { Router, Request, Response } from 'express'
 
 const root: Router = Router()
 
 root.use('/auth', auth)
-root.use('/api', jwtVerify, patients)
+root.use('/api', patients)
 
 root.get('/', (req: Request, res: Response) => {
     res.status(200).json({
