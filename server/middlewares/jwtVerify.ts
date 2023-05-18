04f20cc442bc85b1b1bb1297990fc7b47eb8975b
Raheem Kawojue <kawojue08@gmail.com>
2023-05-18 21:35:43 +0100

    .

--- server/middlewares/jwtVerify.ts
+++ server/middlewares/jwtVerify.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 import User from '../models/User'
-import { ACCESS_DENIED } from '../configs/error'
 import { Response, NextFunction } from 'express'
+import { ACCESS_DENIED } from '../utilities/error'
 const asyncHandler = require('express-async-handler')
 
 const jwtVerify = asyncHandler(async (req: any, res: Response, next: NextFunction) => {
