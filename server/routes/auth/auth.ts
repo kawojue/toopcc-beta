673548546dc6101353289f8b46123636650dffe0
Raheem Kawojue <kawojue08@gmail.com>
2023-05-18 23:07:19 +0100

    ...

---
+++ server/routes/auth/auth.ts
@@ -0,0 +1,27 @@
+import otp from './otp'
+import edit from './edit'
+import { Router } from 'express'
+import {
+    login, logout, createUser, resetpswd,
+} from '../../controllers/userAuth'
+import { ILimiter } from '../../type'
+import limiter from '../../middlewares/limiter'
+
+const auth: Router = Router()
+
+auth.use('/otp', otp)
+auth.use('/edit', edit)
+
+const loginLimiter: ILimiter = {
+    max: 5,
+    timerArr: [23, 32, 19, 52, 42],
+    msg: "Too many attempts. Please, try again later."
+}
+
+auth.get('/logout', logout)
+auth.post('/signup', createUser)
+auth.post('/password/reset', resetpswd)
+auth.post('/login', limiter(loginLimiter), login)
+
+
+export default auth
