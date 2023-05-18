8393ae62496241b71631d81a0971d64e8d71af6d
Raheem Kawojue <kawojue08@gmail.com>
2023-05-18 13:19:32 +0100

    [add] account resigned

--- server/middlewares/jwtVerify.ts
+++ server/middlewares/jwtVerify.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import User from '../models/User'
 import { ACCESS_DENIED } from '../configs/error'
 import { Response, NextFunction } from 'express'
 const asyncHandler = require('express-async-handler')
@@ -13,6 +14,9 @@ const jwtVerify = asyncHandler(async (req: any, res: Response, next: NextFunctio
         process.env.JWT_SECRET as string,
         async (err: any, decoded: any) => {
             if (err) return res.status(403).json(ACCESS_DENIED)
+
+            const account: any = await User.findOne({ user: decoded.userData.user }).exec()
+            if (account.resigned)  return res.status(403).json(ACCESS_DENIED)
             
             req.user = decoded.userData.user
             req.roles = decoded.userData.roles
