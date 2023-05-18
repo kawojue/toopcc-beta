de62a9cf4781a09441efee2e5ff06ab18b4e7a0d
Raheem Kawojue <kawojue08@gmail.com>
2023-05-18 08:46:22 +0100

    error info

--- server/configs/error.ts
+++ server/configs/error.ts
@@ -4,4 +4,18 @@ const ACCESS_DENIED = {
     message: "Access Denied."
 }
 
-export { ACCESS_DENIED }
+const PASSWORD_NOT_MATCH = {
+    success: false,
+    action: "warning",
+    msg: "Password does not match."
+}
+
+const FIELDS_REQUIRED = {
+    success: false,
+    action: "error",
+    message: "All fields are required"
+}
+
+export {
+    ACCESS_DENIED, FIELDS_REQUIRED, PASSWORD_NOT_MATCH,
+}
