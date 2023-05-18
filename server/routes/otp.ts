848c7136c0035f2f3277ae614fbac280ff8f6a72
Raheem Kawojue <kawojue08@gmail.com>
2023-05-18 21:58:36 +0100

    [edit]

--- server/routes/otp.ts
+++ server/routes/otp.ts
@@ -6,7 +6,7 @@ import limiter from '../middlewares/limiter'
 
 const otp: Router = Router()
 
-otp.post('/otp/verify', limiter({ max: 3, timerArr: [14, 9, 15] }), verifyOTP)
-otp.post('/otp/request', limiter({ max: 1, timerArr: [15, 25, 40] }), otpHandler)
+otp.post('/verify', limiter({ max: 3, timerArr: [14, 9, 15] }), verifyOTP)
+otp.post('/request', limiter({ max: 1, timerArr: [15, 25, 40] }), otpHandler)
 
 export default otp
