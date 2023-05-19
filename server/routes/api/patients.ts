14c0ab2e65551eda149032f0056fd18210fbdc0c
Raheem Kawojue <kawojue08@gmail.com>
2023-05-19 13:18:54 +0100

    [add] add patient route

--- server/routes/api/patients.ts
+++ server/routes/api/patients.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
+import { add } from '../../controllers/patients'
 import jwtVerify from '../../middlewares/jwtVerify'
+import verifyRoles from '../../middlewares/verifyRoles'
 
 const patients: Router = Router()
 
-patients.use(jwtVerify)
+patients.use([jwtVerify, verifyRoles("admin")])
+
+patients.post('/add', add)
 
 export default patients
