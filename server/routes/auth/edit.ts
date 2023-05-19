bdd115817821ea67619e5e22f4c08a6a2c56fd42
Raheem Kawojue <kawojue08@gmail.com>
2023-05-19 08:11:48 +0100

    [fix]

--- server/routes/auth/edit.ts
+++ server/routes/auth/edit.ts
@@ -2,11 +2,14 @@ import { Router } from 'express'
 import {
     editUsername, editFullname, editPassword
 } from '../../controllers/userAuth'
+import jwtVerify from '../../middlewares/jwtVerify'
 
 const edit: Router = Router()
+
+edit.use(jwtVerify)
 
 edit.post('/username', editUsername)
 edit.post('/fullname', editFullname)
 edit.post('/password', editPassword)
 
-export default Router
+export default edit
