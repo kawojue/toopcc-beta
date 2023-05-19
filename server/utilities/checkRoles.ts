9381a54fc92afc3a2447dc8630510932242951ae
Raheem Kawojue <kawojue08@gmail.com>
2023-05-19 11:54:28 +0100

    refractored

--- server/utilities/checkRoles.ts
+++ server/utilities/checkRoles.ts
@@ -1,11 +1,12 @@
-export default function checkRoles(authRoles: string[], roles: string[]): boolean{
+export default function checkRoles(authRoles: string[], roles: string[]): boolean {
     if (!authRoles || !roles) {
         return false
     }
 
-    const check: boolean[] = roles.map((role: string) => authRoles.includes(role))
-    if (check.length === roles.length) {
-        return true
+    const result: any = roles.map((role: string) => authRoles.includes(role)).find((value: boolean) => value === true)
+    if (!result) {
+        return false
     }
-    return false
+
+    return true
 }
