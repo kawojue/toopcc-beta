278161bb471c97c2e58bf346c023746a3c774b6b
Raheem Kawojue <kawojue08@gmail.com>
2023-05-21 00:53:50 +0100

    more on del avatar

--- server/controllers/userAuth.ts
+++ server/controllers/userAuth.ts
@@ -342,7 +342,8 @@ const deleteAvatar = asyncHandler(async (req: any, res: Response) => {
     const account: any = await User.findOne({ user: req?.user }).exec()
     if (!account) return res.status(404).json(SMTH_WENT_WRONG)
 
-    const result: any = await cloudinary.uploader.destroy(account.avatar?.public_id)
+    // const result: any = await cloudinary.uploader.destroy(account.avatar?.public_id)
+    const result: any = await cloudinary.api.delete_resources_by_prefix(account.id)
     account.avatar = {
         secure_url: "",
         public_id: ""
