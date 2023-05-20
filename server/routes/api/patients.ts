e03f9036f670167c0d5786c0d943abb039d60b71
Raheem Kawojue <kawojue08@gmail.com>
2023-05-20 16:29:33 +0100

    [add] remove pt route

--- server/routes/api/patients.ts
+++ server/routes/api/patients.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import jwtVerify from '../../middlewares/jwtVerify'
-import { add, edit } from '../../controllers/patients'
+import {
+    add, edit, remove
+} from '../../controllers/patients'
 import verifyRoles from '../../middlewares/verifyRoles'
 
 const patients: Router = Router()
@@ -8,6 +10,7 @@ const patients: Router = Router()
 patients.use([jwtVerify, verifyRoles("admin")])
 
 patients.post('/add', add)
-patients.post('/edit', edit)
+patients.put('/edit', edit)
+patients.delete('/remove', remove)
 
 export default patients
