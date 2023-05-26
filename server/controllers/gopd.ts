f2fdc21463938b6c09d00743ee04b927b22bcaf2
Raheem Kawojue <kawojue08@gmail.com>
2023-05-27 00:50:59 +0100

    [add] get all diag func

--- server/controllers/gopd.ts
+++ server/controllers/gopd.ts
@@ -1,6 +1,6 @@
 import Patient from '../models/Patient'
 import { Request, Response } from 'express'
-import { SUCCESS } from '../utilities/modal'
+import { PATIENT_NOT_EXIST, SUCCESS } from '../utilities/modal'
 const asyncHandler = require('express-async-handler')
 
 const allPatients = asyncHandler(async (req: Request, res: Response) => {
@@ -12,4 +12,15 @@ const allPatients = asyncHandler(async (req: Request, res: Response) => {
     })
 })
 
-export { allPatients }
+const getAllDiagnosis = asyncHandler(async (req: Request, res: Response) => {
+    const { card_no }: any = req.params
+    const patient: any = await Patient.findOne({ card_no })
+    if (!patient) return res.status(400).json(PATIENT_NOT_EXIST)
+
+    res.status(200).json({
+        ...SUCCESS,
+        diagnosis: patient.body
+    })
+})
+
+export { allPatients, getAllDiagnosis }
