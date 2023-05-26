1caa518229fd58fa0be96fd23a2bf3513849c0dd
Raheem Kawojue <kawojue08@gmail.com>
2023-05-27 00:46:53 +0100

    [edit] route

--- server/routes/root.ts
+++ server/routes/root.ts
@@ -1,11 +1,13 @@
 import auth from './auth/auth'
 import patients from './api/setPatient'
+import getPatients from './api/getPatient'
 import { Router, Request, Response } from 'express'
 
 const root: Router = Router()
 
 root.use('/auth', auth)
 root.use('/api/patients', patients)
+root.use('/api/getpts', getPatients)
 
 root.get('/', (req: Request, res: Response) => {
     res.status(200).json({
