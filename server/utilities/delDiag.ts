c24ccec8221a95d6c1cb5db09d76b72a1b7da41b
Raheem Kawojue <kawojue08@gmail.com>
2023-05-26 09:29:47 +0100

    [fixed]

--- server/utilities/delDiag.ts
+++ server/utilities/delDiag.ts
@@ -1,10 +1,10 @@
-import { ICloud } from "../type"
+import { ICloud, IBody } from "../type"
 import cloudinary from "../configs/cloudinary"
 
 export default async function delDiag(bodies: any[]): Promise<boolean> {
     if (bodies.length > 0) {
-        bodies.forEach((body: any) => {
-            const images: ICloud[] = body.images
+        bodies.forEach((body: IBody) => {
+            const images: ICloud[] = body.diagnosis.images
             if (images.length > 0) {
                 images.forEach(async (image: ICloud) => {
                     const result = await cloudinary.uploader.destroy(image.public_id)
