12f2563c121bd17539aecf00af02c9e25d3e0d0d
Raheem Kawojue <kawojue08@gmail.com>
2023-05-27 00:07:42 +0100

    ..

--- server/controllers/gopd.ts
+++ server/controllers/gopd.ts
@@ -2,11 +2,11 @@ import Patient from '../models/Patient'
 import { Request, Response } from 'express'
 const asyncHandler = require('express-async-handler')
 
-const all = asyncHandler(async (req: Request, res: Response) => {
+const allPatients = asyncHandler(async (req: Request, res: Response) => {
     const patients = await Patient.find().exec()
     res.status(200).json({
         patients
     })
 })
 
-export { all }
+export { allPatients }
