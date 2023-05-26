b6fcd36984384bc74a6ab0a157c5a5107ce0e9a2
Raheem Kawojue <kawojue08@gmail.com>
2023-05-26 23:43:28 +0100

    fixed

--- server/routes/api/patients.ts
+++ server/routes/api/patients.ts
@@ -27,7 +27,7 @@ patients.route('/diagnosis/:card_no/:idx')
 
 // update and delete patient referral or recommendation routes
 patients.put('/recommendation/:card_no', addRecommendation)
-patients.delete('/recommendation/:card_no/:type/:idx', addRecommendation)
+patients.delete('/recommendation/:card_no/:type/:idx', deleteRecommendation)
 
 // update and delete patient extension routes
 patients.route('/recommendation/extension/:card_no/:idx')
