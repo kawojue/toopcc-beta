189fd5c5a7572f01ec992b0e1b6b12e9a32288eb
Raheem Kawojue <kawojue08@gmail.com>
2023-05-27 00:42:58 +0100

    fetch all patients

--- server/controllers/gopd.ts
+++ server/controllers/gopd.ts
@@ -1,10 +1,13 @@
 import Patient from '../models/Patient'
 import { Request, Response } from 'express'
+import { SUCCESS } from '../utilities/modal'
 const asyncHandler = require('express-async-handler')
 
 const allPatients = asyncHandler(async (req: Request, res: Response) => {
-    const patients = await Patient.find().exec()
+    const patients: any = await Patient.find().select('-body -recommendation').exec()
+    console.log(patients)
     res.status(200).json({
+        ...SUCCESS,
         patients
     })
 })
