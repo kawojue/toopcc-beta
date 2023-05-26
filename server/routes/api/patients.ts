91a6bbef8a95224eb5babf0566b74e4a3756f9c1
Raheem Kawojue <kawojue08@gmail.com>
2023-05-26 23:24:50 +0100

    update route

--- server/routes/api/patients.ts
+++ server/routes/api/patients.ts
@@ -10,23 +10,26 @@ import verifyRoles from '../../middlewares/verifyRoles'
 
 const patients: Router = Router()
 
+// only admin and hr have access to do these.
 patients.use([jwtVerify, verifyRoles("admin", "hr")])
 
+// add, edit, and delete patient data routes
 patients.post('/add', add)
 patients.route('/:card_no')
     .put(edit)
     .delete(remove)
 
+// add, edit, and delete patient diagnosis routes
 patients.post('/diagnosis/:card_no', addDiagnosis)
-
 patients.route('/diagnosis/:card_no/:idx')
     .put(editDiagnosis)
     .delete(deleteDianosis)
 
-patients.route('/recommendation/:card_no')
-    .put(addRecommendation)
-    .delete(deleteRecommendation)
+// update and delete patient referral or recommendation routes
+patients.put('/recommendation/:card_no', addRecommendation)
+patients.delete('/recommendation/:card_no/:type/:idx', addRecommendation)
 
+// update and delete patient extension routes
 patients.route('/recommendation/extension/:card_no/:idx')
     .put(editExtension)
     .delete(deletExtension)
