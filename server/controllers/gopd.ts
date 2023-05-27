063b8ecde62a2ece80c71a99f51e2a96da77da2f
Raheem Kawojue <kawojue08@gmail.com>
2023-05-28 00:08:30 +0100

    [add] get patient & diag func

--- server/controllers/gopd.ts
+++ server/controllers/gopd.ts
@@ -1,26 +1,62 @@
+import { IBody } from '../type'
 import Patient from '../models/Patient'
 import { Request, Response } from 'express'
-import { PATIENT_NOT_EXIST, SUCCESS } from '../utilities/modal'
+import {
+    PATIENT_NOT_EXIST, SUCCESS,
+    DIAG_NOT_EXIST
+} from '../utilities/modal'
 const asyncHandler = require('express-async-handler')
 
 const allPatients = asyncHandler(async (req: Request, res: Response) => {
-    const patients: any = await Patient.find().select('-body -recommendation').exec()
-    console.log(patients)
+    const patients: any = await Patient.find()
+    .select('-body -recommendation').exec()
+
     res.status(200).json({
         ...SUCCESS,
         patients
     })
 })
 
-const getAllDiagnosis = asyncHandler(async (req: Request, res: Response) => {
+const getPatient = asyncHandler(async (req: Request, res: Response) => {
     const { card_no }: any = req.params
     const patient: any = await Patient.findOne({ card_no })
-    if (!patient) return res.status(400).json(PATIENT_NOT_EXIST)
+    .select('-body -recommendation').exec()
+    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
 
     res.status(200).json({
         ...SUCCESS,
-        diagnosis: patient.body
+        patient
     })
 })
 
-export { allPatients, getAllDiagnosis }
+const getAllDiagnosis = asyncHandler(async (req: Request, res: Response) => {
+    const { card_no }: any = req.params
+    const patient: any = await Patient.findOne({ card_no }).exec()
+    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+
+    res.status(200).json({
+        ...SUCCESS,
+        length: patient.body.length,
+        diagnosis: patient.body.reverse()
+    })
+})
+
+const getDiagnosis = asyncHandler(async (req: Request, res: Response) => {
+    const { card_no, idx }: any = req.params
+    const patient: any = await Patient.findOne({ card_no }).exec()
+    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+
+    const bodies: any[] = patient.body
+    const diagnosis: any = bodies.find((body: IBody) => body.idx === idx)
+    if (!diagnosis) return res.status(404).json(DIAG_NOT_EXIST)
+
+    res.status(200).json({
+        ...SUCCESS,
+        diagnosis
+    })
+})
+
+export {
+    allPatients, getAllDiagnosis,
+    getDiagnosis, getPatient,
+}
