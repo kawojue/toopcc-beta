e7d4282900546a49d4ef4aca0bf989e685b936dd
Raheem Kawojue <kawojue08@gmail.com>
2023-05-28 00:15:14 +0100

    [add] get diag route

--- server/routes/api/getPatient.ts
+++ server/routes/api/getPatient.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import {
-    allPatients, getAllDiagnosis, getDiagnosis
+    allPatients, getAllDiagnosis, getDiagnosis,
+    getPatient
 } from '../../controllers/gopd'
 import jwtVerify from '../../middlewares/jwtVerify'
 import verifyRoles from '../../middlewares/verifyRoles'
@@ -10,7 +11,8 @@ const getPatients: Router = Router()
 getPatients.use([jwtVerify, verifyRoles("staff", "admin", "hr")])
 
 getPatients.get('/all', allPatients)
-getPatients.get('/diagnosis/card_no', getDiagnosis)
-getPatients.get('/diagnosis/all/card_no', getAllDiagnosis)
+getPatients.get('/:card_no', allPatients)
+getPatients.get('/diagnosis/:card_no', getDiagnosis)
+getPatients.get('/diagnosis/all/:card_no', getAllDiagnosis)
 
 export default getPatients
