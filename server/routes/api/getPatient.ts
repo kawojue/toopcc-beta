fa91b7e94a52cb5864cb34cf7d766baa7da0f8ba
Raheem Kawojue <kawojue08@gmail.com>
2023-05-28 00:23:39 +0100

    [edit]

--- server/routes/api/getPatient.ts
+++ server/routes/api/getPatient.ts
@@ -10,9 +10,9 @@ const getPatients: Router = Router()
 
 getPatients.use([jwtVerify, verifyRoles("staff", "admin", "hr")])
 
-getPatients.get('/all', allPatients)
+getPatients.get('/', allPatients)
 getPatients.get('/:card_no', getPatient)
-getPatients.get('/diagnosis/:card_no', getDiagnosis)
-getPatients.get('/diagnosis/all/:card_no', getAllDiagnosis)
+getPatients.get('/diagnosis/:card_no', getAllDiagnosis)
+getPatients.get('/diagnosis/:card_no/:idx', getDiagnosis)
 
 export default getPatients
