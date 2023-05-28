74dce1697b44bbffdaffccff4a9b3fee87cfece9
Raheem Kawojue <kawojue08@gmail.com>
2023-05-28 23:24:17 +0100

    [add] pt fetching

--- server/utilities/pts.ts
+++ server/utilities/pts.ts
@@ -4,9 +4,10 @@ const fetchByCardNumber = async (card_no: string, omit: string = ""): Promise<an
     let patient: any
 
     if (omit) {
+        patient = await Patient.findOne({ card_no }).exec()
+    } else {
         patient = await Patient.findOne({ card_no }).select(omit).exec()
     }
-    patient = await Patient.findOne({ card_no }).exec()
 
     return patient
 }
@@ -16,8 +17,9 @@ const fetchPatients = async(omit: string = ""): Promise<any[]> => {
 
     if (omit) {
         patients = await Patient.find().select(omit).exec()
+    } else {
+        patients = await Patient.find().exec()
     }
-    patients = await Patient.find().exec()
 
     return patients
 }
