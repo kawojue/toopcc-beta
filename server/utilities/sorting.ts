e0592918c85322632dbf07e86cf7d969e5f8c3d1
Raheem Kawojue <kawojue08@gmail.com>
2023-05-28 12:47:25 +0100

    [add] sorting

---
+++ server/utilities/sorting.ts
@@ -0,0 +1,28 @@
+const sortByDates = (array: any[]) => {
+    return array.sort((a, b) => {
+        const dateA: any = new Date(a.date)
+        const dateB: any = new Date(b.date)
+
+        return dateA - dateB;
+    })
+}
+
+const sortByCardNumbers = (array: any[]) => {
+    return array.sort((a, b) => {
+        const numA = parseInt(a.card_no, 10)
+        const numB = parseInt(b.card_no, 10)
+
+        if (numA < numB) {
+            return -1
+        } else if (numA > numB) {
+            return 1
+        }
+
+        const alphaA = a.replace(/[^a-zA-Z]/g, "")
+        const alphaB = b.replace(/[^a-zA-Z]/g, "")
+
+        return alphaA.localeCompare(alphaB)
+    })
+}
+
+export { sortByCardNumbers, sortByDates }
