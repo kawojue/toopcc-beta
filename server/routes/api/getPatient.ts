b7282be91f0183652d5ebe96817172a74e1c6aa5
Raheem Kawojue <kawojue08@gmail.com>
2023-05-28 13:37:41 +0100

    [add] ext routes

--- server/routes/api/getPatient.ts
+++ server/routes/api/getPatient.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import {
-    allPatients, getAllDiagnosis, getDiagnosis,
-    getPatient, getAllPhysioPatients,
-    getDeadPatients, getAllOpthalPatients,
+    allPatients, getAllDiagnosis, getExtension,
+    getDiagnosis, getPatient, getAllExtensions,
+    getDeadPatients, getAllOpthalPatients, getAllPhysioPatients
 } from '../../controllers/gopd'
 import jwtVerify from '../../middlewares/jwtVerify'
 import verifyRoles from '../../middlewares/verifyRoles'
@@ -18,5 +18,9 @@ getPatients.get('/diagnosis/:card_no', getAllDiagnosis)
 getPatients.get('/diagnosis/:card_no/:idx', getDiagnosis)
 getPatients.get('/recommendation/opthal', getAllOpthalPatients)
 getPatients.get('/recommendation/physio', getAllPhysioPatients)
+getPatients.get('/recommendation/opthal/medications', getAllOpthalPatients)
+getPatients.get('/recommendation/physio/medications', getAllPhysioPatients)
+getPatients.get('/recommendation/extensions', getAllExtensions)
+getPatients.get('/recommendation/extensions/:card_no', getExtension)
 
 export default getPatients
