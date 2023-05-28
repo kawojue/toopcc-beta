9633d3302bfe8cc5b10ff2213db936bc345942e9
Raheem Kawojue <kawojue08@gmail.com>
2023-05-28 23:43:06 +0100

    [fix] routes

--- server/routes/api/getPatient.ts
+++ server/routes/api/getPatient.ts
@@ -20,9 +20,9 @@ getPatients.get('/diagnosis/:card_no', getAllDiagnosis)
 getPatients.get('/diagnosis/:card_no/:idx', getDiagnosis)
 getPatients.get('/recommendation/opthal', getAllOpthalPatients)
 getPatients.get('/recommendation/physio', getAllPhysioPatients)
-getPatients.get('/recommendation/opthal/medications', getOpthalMedication)
-getPatients.get('/recommendation/physio/medications', getPhysioMedication)
 getPatients.get('/recommendation/extensions', getAllExtensions)
 getPatients.get('/recommendation/extensions/:card_no', getExtension)
+getPatients.get('/recommendation/opthal/medications/:card_no', getOpthalMedication)
+getPatients.get('/recommendation/physio/medications/:card_no', getPhysioMedication)
 
 export default getPatients
