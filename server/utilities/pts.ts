67e9a1d2a5cb0affe4e6b79348b2ed9860549b21
Raheem Kawojue <kawojue08@gmail.com>
2023-05-30 09:47:06 +0100

    refractored

--- server/utilities/pts.ts
+++ server/utilities/pts.ts
@@ -1,27 +1,17 @@
 import Patient from "../models/Patient"
 
 const fetchByCardNumber = async (card_no: string, omit: string = ""): Promise<any> => {
-    let patient: any
-
-    if (omit) {
-        patient = await Patient.findOne({ card_no }).exec()
-    } else {
-        patient = await Patient.findOne({ card_no }).select(omit).exec()
+    if (omit === "") {
+        return await Patient.findOne({ card_no }).exec()
     }
-
-    return patient
+    return await Patient.findOne({ card_no }).select(omit).exec()
 }
 
 const fetchPatients = async(omit: string = ""): Promise<any[]> => {
-    let patients: any
-
-    if (omit) {
-        patients = await Patient.find().select(omit).exec()
-    } else {
-        patients = await Patient.find().exec()
+    if (omit === "") {
+        return await Patient.find().exec()
     }
-
-    return patients
+    return await Patient.find().select(omit).exec()
 }
 
 export { fetchByCardNumber, fetchPatients }
