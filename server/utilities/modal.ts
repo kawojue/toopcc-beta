ac644309d9da50d12f00e4a958a700944dbc6c2f
Raheem Kawojue <kawojue08@gmail.com>
2023-05-30 13:30:30 +0100

    [rem] error type

--- server/utilities/modal.ts
+++ server/utilities/modal.ts
@@ -2,8 +2,6 @@ const ERROR = { success: false, action: "error" }
 
 const SUCCESS = { success: true, action: "success" }
 
-const WARNING = { success: false, action: "warning" }
-
 const ACCESS_DENIED = { ...ERROR, msg: "Access Denied" }
 
 const INVALID_EMAIL = { ...ERROR, msg: "Invalid Email" }
@@ -16,29 +14,29 @@ const FIELDS_REQUIRED = { ...ERROR, msg: "All fields are required" }
 
 const ACCOUNT_NOT_FOUND = { ...ERROR, msg: "Account does not exist." }
 
-const PSWD_NOT_MATCH = { ...WARNING, msg: "Password does not match." }
+const PSWD_NOT_MATCH = { ...ERROR, msg: "Password does not match." }
 
-const CURRENT_PSWD = { ...WARNING, msg: "Not saved! Same password." }
+const CURRENT_PSWD = { ...ERROR, msg: "Not saved! Same password." }
 
-const SEX_REQUIRED = { ...WARNING, msg: "Sex is required." }
+const SEX_REQUIRED = { ...ERROR, msg: "Sex is required." }
 
 const PATIENT_NOT_EXIST = { ...ERROR, msg: "Patient does not exist." }
 
 const CARD_NO_REQUIRED = { ...ERROR, msg: "Card number is required. "}
 
-const INVALID_PHONE_NO = { ...WARNING, msg: "Invalid phone number" }
+const INVALID_PHONE_NO = { ...ERROR, msg: "Invalid phone number" }
 
-const INVALID_AGE = { ...WARNING, msg: "Age is not valid" }
+const INVALID_AGE = { ...ERROR, msg: "Age is not valid" }
 
 const PSWD_CHANGED = { ...SUCCESS, msg: "Password updated successfully." }
 
 const SAVED = { ...SUCCESS, msg: "Saved!" }
 
-const PATIENT_EXIST = { ...WARNING, msg: "Patient with card number already exists." }
+const PATIENT_EXIST = { ...ERROR, msg: "Patient with card number already exists." }
 
 const DELETION_FAILED = {...ERROR, msg: "Failed to delete!" }
 
-const CANCELED = { ...WARNING, msg: "Canceled." }
+const CANCELED = { ...ERROR, msg: "Canceled." }
 
 const EXT_NOT_EXIST = { ...ERROR, msg: "Extension does not exist. "}
 
@@ -48,7 +46,7 @@ const DIAG_NOT_EXIST = { ...ERROR, msg: "Diagnosis does not exist." }
 
 export {
     FIELDS_REQUIRED, INVALID_EMAIL, ACCESS_DENIED, SUCCESS,
-    PSWD_NOT_MATCH, ACCOUNT_NOT_FOUND, ERROR, WARNING,
+    PSWD_NOT_MATCH, ACCOUNT_NOT_FOUND, ERROR,
     CURRENT_PSWD, INCORRECT_PSWD, PSWD_CHANGED,
     SMTH_WENT_WRONG, SEX_REQUIRED, CARD_NO_REQUIRED,
     INVALID_AGE, INVALID_PHONE_NO, PATIENT_NOT_EXIST,
