b4dfd840e52082c148a4570bac7286478f42e4ba
Raheem Kawojue <kawojue08@gmail.com>
2023-05-31 12:16:47 +0100

    [fix] patients page

---
+++ client/app/patients/page.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import { Suspense } from 'react'
+import type { Metadata } from 'next'
+import useAuth from '@/hooks/useAuth'
+import axios from '@/app/api/instance'
+import { useEffect, useState } from 'react'
+import Patients from '@/components/Patients'
+import { SpinnerTwo } from "@/components/Spinner"
+import throwError from '@/utilities/throwError'
+
+export const metadata: Metadata = {
+    title: "Patients",
+    description: "All Patients Data."
+}
+
+const page = async () => {
+    const { token }: any = useAuth()
+    const [patients, setPatients] = useState<any[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const getAllPatients = async (token: string) => {
+        setLoading(true)
+        await axios.get(`/api/patients`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then((res: any) => {
+            setPatients(res.data?.patients)
+        })
+        .catch((err: any) => throwError(err)).finally(() => setLoading(false))
+    }
+
+    useEffect(() => {
+        (async () => await getAllPatients(token))()
+    }, [token])
+
+    if (loading) return <SpinnerTwo />
+
+    return (
+        <>
+            <Suspense fallback={<SpinnerTwo />}>
+                <Patients patients={patients}/>
+            </Suspense>
+        </>
+    )
+}
+
+export default page
