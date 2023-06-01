9da72001fc1c813302b9643a5225c45bd9a6d326
Raheem Kawojue <kawojue08@gmail.com>
2023-06-01 11:11:33 +0100

    refractored

--- client/components/Spinner.tsx
+++ client/components/Spinner.tsx
@@ -1,12 +1,10 @@
 const SpinnerOne: React.FC = () => {
     return (
-        <main className="w-full h-full flex justify-center mt-14">
-            <article className="loading-spin">
-                <div className="spin-sector spin-sector-1"></div>
-                <div className="spin-sector spin-sector-2"></div>
-                <div className="spin-sector spin-sector-3"></div>
-            </article>
-        </main>
+        <article className="loading-spin">
+            <div className="spin-sector spin-sector-1"></div>
+            <div className="spin-sector spin-sector-2"></div>
+            <div className="spin-sector spin-sector-3"></div>
+        </article>
     )
 }
 
