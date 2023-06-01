34705891c7d0b493a009174ec0c1428f11a2d212
Raheem Kawojue <kawojue08@gmail.com>
2023-06-01 11:09:48 +0100

    refractored

---
+++ client/utils/file.ts
@@ -0,0 +1,29 @@
+import notify from "@/utils/notify"
+
+const checkFile = (file: any): boolean => {
+    if (!file) return false
+    const maxSize: number = 3145728 // 3MB
+    const { name, size }: any = file
+    const allowedFormats: string[] = ['jpg', 'png']
+    const split: string[] = name.split('.')
+    const extension: string = split[split.length - 1]
+    if (allowedFormats.includes(extension) && size <= maxSize) {
+        return true
+    }
+    return false
+}
+
+const handleFile = (e: any, set: any) => {
+    const file: any = e.target.files[0]
+    const reader: FileReader = new FileReader()
+    if (checkFile(file)) {
+        reader.readAsDataURL(file)
+        reader.onload = () => {
+            set(reader.result)
+        }
+    } else {
+        notify('error', "File size or format is not allowed.")
+    }
+}
+
+export { checkFile, handleFile }
