6c842d035978070cddffd50e767ede07c42cedac
Raheem Kawojue <kawojue08@gmail.com>
2023-06-02 17:28:47 +0100

    [add] get User model

---
+++ server/utilities/getModels.ts
@@ -0,0 +1,37 @@
+import User from '../models/User'
+import Patient from "../models/Patient"
+
+const fetchByCardNumber = async (card_no: string, omit: string = ""): Promise<any> => {
+    if (omit === "") {
+        return await Patient.findOne({ card_no }).exec()
+    }
+    return await Patient.findOne({ card_no }).select(omit).exec()
+}
+
+const fetchUserByUser = async (user: string, omit: string = ""): Promise<any> => {
+    if (omit === "") {
+        return await User.findOne({ user }).exec()
+    }
+    return await User.findOne({ user }).select(omit).exec()
+}
+
+const fetchPatients = async(omit: string = ""): Promise<any[]> => {
+    if (omit === "") {
+        return await Patient.find().exec()
+    }
+    return await Patient.find().select(omit).exec()
+}
+
+const fetchUsers = async(omit: string = ""): Promise<any[]> => {
+    if (omit === "") {
+        return await User.find().exec()
+    }
+    return await User.find().select(omit).exec()
+}
+
+
+
+export {
+    fetchUserByUser, fetchUsers,
+    fetchByCardNumber, fetchPatients,
+}
