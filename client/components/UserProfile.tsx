bc2ab002d7e8d3b19a0b0cf46c8b9800d24d2a1d
Raheem Kawojue <kawojue08@gmail.com>
2023-06-03 00:32:41 +0100

    ..

--- client/components/UserProfile.tsx
+++ client/components/UserProfile.tsx
@@ -1,7 +1,11 @@
-
+"use client"
 const UserProfile: React.FC<any> = ({ profile }) => {
     return (
         <main className="profile-main">
+            <article className="profile-header">
+                <h1>Your Info</h1>
+                <button></button>
+            </article>
         </main>
     )
 }
