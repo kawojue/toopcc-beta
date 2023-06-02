ffd44c81161faa551fa51fc90eea475fe297dc13
Raheem Kawojue <kawojue08@gmail.com>
2023-06-02 17:49:58 +0100

    [add] getUser func

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,3 +1,16 @@
-import User from '../models/User'
+import {
+    fetchUsers, fetchUserByUser
+} from '../utilities/getModels'
+import { Response } from 'express'
+import { SUCCESS } from '../utilities/modal'
+import expressAsyncHandler from 'express-async-handler'
 
+const getUser = expressAsyncHandler(async (req: any, res: Response) => {
+    const profile: any = await fetchUserByUser(req?.user, '-token -password -OTP')
+    res.status(200).json({
+        ...SUCCESS,
+        profile
+    })
+})
 
+export { getUser }
