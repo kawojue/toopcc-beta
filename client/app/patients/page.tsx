6ffc77f19ae42256dada8b68ddffe014c7adb144
Raheem Kawojue <kawojue08@gmail.com>
2023-06-02 10:27:22 +0100

    optimized

--- client/app/patients/page.tsx
+++ client/app/patients/page.tsx
@@ -1,43 +1,41 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import type { Metadata } from 'next'
 import useAuth from '@/hooks/useAuth'
 import axios from '@/app/api/instance'
-import throwError from '@/utils/throwError'
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import throwError from '@/utils/throwError'
 import Patients from '@/components/Patients'
-import { SpinnerTwo } from "@/components/Spinner"
-
-export const metadata: Metadata = {
-    title: "Patients",
-    description: "All Patients Data."
-}
+import { SpinnerTwo } from '@/components/Spinner'
 
 const page = () => {
-    const { token }: any = useAuth()
+    const router = useRouter()
+    const { token, auth }: any = useAuth()
     const [patients, setPatients] = useState<any[]>([])
-    const [loading, setLoading] = useState<boolean>(false)
+    const [loading, setLoading] = useState<boolean>(true)
 
     const getAllPatients = async (token: string) => {
-        setLoading(true)
         await axios.get(`/api/patients`, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
-        })
-        .then((res: any) => {
-            setPatients(res.data?.patients)
-        })
-        .catch((err: any) => throwError(err)).finally(() => setLoading(false))
+        }).then((res: any) => setPatients(res.data?.patients))
+        .catch((err: any) => {
+            throwError(err)
+            setTimeout(() => {
+                router.push('/staff/login')
+            }, 1500)
+        }).finally(() => setLoading(false))
     }
 
     useEffect(() => {
-        if (token) (async () => await getAllPatients(token))()
-    }, [token])
+        if (auth) (async () => await getAllPatients(token))()
+    }, [token, auth])
 
     if (loading) return <SpinnerTwo />
 
-    return <Patients patients={patients}/>
+    return <Patients patients={patients} />
 }
 
 export default page
