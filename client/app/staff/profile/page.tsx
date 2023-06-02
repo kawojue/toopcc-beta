f9145bf4097da907cb57f0c582da81efd7bed54a
Raheem Kawojue <kawojue08@gmail.com>
2023-06-03 00:22:49 +0100

    [add] prrofile page setup

--- client/app/staff/profile/page.tsx
+++ client/app/staff/profile/page.tsx
@@ -1,7 +1,15 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import useAuth from '@/hooks/useAuth'
+import { SpinnerTwo } from '@/components/Spinner'
+import UserProfile from '@/components/UserProfile'
+
 const page = () => {
-    return (
-        <></>
-    )
+    const { profile, loadingProfile }: any = useAuth()
+    
+    if (loadingProfile) return <SpinnerTwo />
+
+    return <UserProfile profile={profile} />
 }
 
 export default page
