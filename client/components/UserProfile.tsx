e2f2c6babb56ae48c3b8cb373173a7c11f2cc384
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 12:00:08 +0100

    more on user profile

--- client/components/UserProfile.tsx
+++ client/components/UserProfile.tsx
@@ -4,12 +4,21 @@ import { GoKey } from '@/public/icons/ico'
 const UserProfile: React.FC<any> = ({ profile }) => {
     return (
         <main className="profile-main">
-            <article className="profile-header">
+            <section className="profile-header">
                 <h1>Your Info</h1>
                 <button>
-
+                    <GoKey />
+                    <p>
+                        <span>Change Password</span>
+                        <span>Security</span>
+                    </p>
                 </button>
-            </article>
+            </section>
+            <section className="profile-cards">
+                <article className="profile-card">
+
+                </article>
+            </section>
         </main>
     )
 }
