a2bcfd47f0e51fe103f617c419979087486a1bae
Raheem Kawojue <kawojue08@gmail.com>
2023-06-08 12:37:54 +0100

    more on profile

--- client/components/UserProfile.tsx
+++ client/components/UserProfile.tsx
@@ -1,7 +1,13 @@
 "use client"
-import { HiOutlineKey } from '@/public/icons/ico'
+import Image from 'next/image'
+import { useState } from 'react'
+import {
+    HiOutlineKey, AiOutlineCamera, AiOutlineMail
+} from '@/public/icons/ico'
 
 const UserProfile: React.FC<{ profile: any }> = ({ profile }) => {
+    const [onMouse, setOnMouse] = useState(false)
+
     return (
         <main className="profile-main">
             <section className="profile-header">
@@ -16,7 +22,22 @@ const UserProfile: React.FC<{ profile: any }> = ({ profile }) => {
             </section>
             <section className="profile-cards">
                 <article className="profile-card">
-
+                    <div className="profile-card-center">
+                        <div onMouseLeave={() => setOnMouse(false)} onMouseEnter={() => setOnMouse(true)}
+                        className={`${onMouse ? 'before:content-[""] before:bg-clr-10 before:absolute before:top-0 before:right-0 before:w-full before:h-full before:z-[999]' : ''} profile-avatar md:w-[12rem] md:h-[12rem]`}>
+                            <Image src={profile?.avatar?.secure_url} alt="avatar"
+                            title="avatar" width={300} height={300} priority/>
+                        </div>
+                        <div className="profile-ids md:gap-16">
+                            <h3 className='profile-ids-h3'>
+                                {profile?.fullname}
+                            </h3>
+                            <p className='profile-ids-p md:text-xs hover:underline trans'>
+                                <AiOutlineMail />
+                                <span>{profile?.mail?.email}</span>
+                            </p>
+                        </div>
+                    </div>
                 </article>
             </section>
         </main>
