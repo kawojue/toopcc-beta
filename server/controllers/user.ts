485ef099effe42459c74454c88b8e4bd942465fe
Raheem Kawojue <kawojue08@gmail.com>
2023-06-09 12:36:21 +0100

    [add] more apis

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -5,12 +5,26 @@ import { Response } from 'express'
 import { SUCCESS } from '../utilities/modal'
 import expressAsyncHandler from 'express-async-handler'
 
-const getUser = expressAsyncHandler(async (req: any, res: Response) => {
+const getProfile = expressAsyncHandler(async (req: any, res: Response) => {
     const profile: any = await fetchUserByUser(req?.user, '-token -password -OTP')
-    res.status(200).json({
-        ...SUCCESS,
-        profile
-    })
+    
+    res.status(200).json({ ...SUCCESS, profile })
 })
 
-export { getUser }
+const getUsers = expressAsyncHandler(async (req: any, res: Response) => {
+    const users: any = await fetchUsers()
+    const names: string[] = users.map((user: any) => {
+        return { username: user.user, fullname: user.fullname }
+    })
+
+    res.status(200).json({ ...SUCCESS, names })
+})
+
+const getUser = expressAsyncHandler(async (req: any, res: Response) => {
+    const { user: userParam }: any = req.params
+    const user: any = await fetchUserByUser(userParam)
+    
+    res.status(200).json({ ...SUCCESS, user })
+})
+
+export { getProfile, getUser, getUsers }
