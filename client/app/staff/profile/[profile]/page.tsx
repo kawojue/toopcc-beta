bb466c525af7ed433fa52d42e47011b17959ec4e
Raheem Kawojue <kawojue08@gmail.com>
2023-06-09 12:52:26 +0100

    [on] staff profile

--- client/app/staff/profile/[profile]/page.tsx
+++ client/app/staff/profile/[profile]/page.tsx
@@ -1,22 +1,46 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import { useEffect } from "react"
 import useJWT from "@/hooks/useJWT"
 import useAuth from "@/hooks/useAuth"
+import axios from "@/app/api/instance"
+import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
+import throwError from "@/utils/throwError"
+import { SpinnerTwo } from "@/components/Spinner"
+import StaffProfile from "@/components/StaffProfile"
 
 const page = ({ params: { profile } } : IProfile) => {
     const router = useRouter()
+
     const { token }: any = useAuth()
     const { roles } : JWT = useJWT() || { roles: [] }
+
+    const [staff, setStaff] = useState<any>({})
+    const [loadingProfile, setLoadingProfile] = useState<boolean>(false)
+
+    const handleStaff = async (token: string): Promise<void> => {
+        setLoadingProfile(true)
+        await axios.get(`/api/user/profile/${profile}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then((res: any) => {
+            setStaff(res.data?.user)
+        }).catch((err: any) => throwError(err)).finally(() => setLoadingProfile(false))
+    }
 
     useEffect(() => {
         if (!roles.includes("hr")) router.push('/staff/profile')
     }, [roles, router])
 
-    return (
-        <></>
-    )
+    useEffect(() => {
+        (async () => await handleStaff(token))()
+    }, [token])
+
+    if (loadingProfile) return <SpinnerTwo />
+
+    return <StaffProfile profile={staff} />
 }
 
 export default page
