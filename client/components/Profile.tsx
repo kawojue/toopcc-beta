52b38eddec20e881914c69bbee18a382a1eb638c
Raheem Kawojue <kawojue08@gmail.com>
2023-06-10 13:52:32 +0100

    [dispatch] states

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from 'next/image'
-import useRoles from '@/hooks/useRoles'
+import useAuth from '@/hooks/useAuth'
+import useRole from '@/hooks/useRole'
 import { inter } from '@/public/font/font'
 import { useState, useEffect } from 'react'
 import { usePathname } from 'next/navigation'
@@ -10,14 +11,17 @@ import {
 } from '@/public/icons/ico'
 
 const Profile: React.FC<{ profile: any }> = ({ profile }) => {
-    const isRole = useRoles("hr")
+    const isRole: boolean = useRole("hr")
     const pathName: string = usePathname()
-    const [onMouse, setOnMouse] = useState(false)
+    const { state, dispatch }: any = useAuth()
+    const [onMouse, setOnMouse] = useState<boolean>(false)
     const [shortDate, setShortDate] = useState<string>("")
     
     useEffect(() => {
         setShortDate(convertISODate(profile?.createdAt))
     }, [profile])
+
+    console.log(state)
 
     const authRoles: string[] = profile?.roles || []
 
@@ -25,9 +29,11 @@ const Profile: React.FC<{ profile: any }> = ({ profile }) => {
         <main className="profile-main">
             <section className="profile-header">
                 <h1 className='profile-header-h1 md:text-3xl'>
-                    {isRole && pathName === "/staff/profile" ? "Your Info": "Staff Info"}
+                    {pathName === "/staff/profile" ? "Your Info": "Staff Info"}
                 </h1>
-                {pathName === "/staff/profile" && <button className="change-pswd-btn">
+                {pathName === "/staff/profile" &&
+                <button className="change-pswd-btn"
+                onClick={() => dispatch({ type: "PSWD", toggle: true })}>
                     <HiOutlineKey className="key" />
                     <div>
                         <span>Change Password</span>
@@ -39,19 +45,25 @@ const Profile: React.FC<{ profile: any }> = ({ profile }) => {
                 <article className="profile-card">
                     <div className="profile-card-center">
                         {isRole && pathName !== "/staff/profile"  ?
-                        <div onMouseLeave={() => setOnMouse(false)} onMouseEnter={() => setOnMouse(true)}
+                        <div onMouseLeave={() => setOnMouse(false)}
+                        onMouseEnter={() => setOnMouse(true)}
                         className={`profile-avatar md:w-[12rem] md:h-[12rem]`}>
                             <Image src={profile?.avatar?.secure_url} alt="avatar"
                             title="change your avatar" width={300} height={300} priority/>
                         </div> :
-                        <div onMouseLeave={() => setOnMouse(false)} onMouseEnter={() => setOnMouse(true)}
+                        <div onMouseLeave={() => setOnMouse(false)}
+                        onMouseEnter={() => setOnMouse(true)}
+                        onClick={() => dispatch({ type: "AVATAR", toggle: true })}
                         className={`${onMouse && 'before:content-[""] before:bg-clr-10 before:absolute before:top-0 before:right-0 before:w-full before:h-full before:z-[999] cursor-pointer'} profile-avatar md:w-[12rem] md:h-[12rem]`}>
                             <Image src={profile?.avatar?.secure_url} alt="avatar"
                             title="avatar" width={300} height={300} priority/>
-                            <AiOutlineCamera className="cam-ico" />
+                            <div className={`${onMouse && 'cam-ico'}`}>
+                                <AiOutlineCamera className="text-clr-0 text-4xl md:text-5xl lg:text-6xl" />
+                            </div>
                         </div>}
                         <div>
-                            <h3 title="edit fullname" className='leading-tight font-semibold cursor-pointer text-clr-2 text-lg md:text-2xl lg:text-4xl hover:underline tracking-wider trans'>
+                            <h3 title="edit fullname" onClick={() => dispatch({ type: "FULLNAME", toggle: true })}
+                            className='leading-tight font-semibold cursor-pointer text-clr-2 text-lg md:text-2xl lg:text-4xl hover:underline tracking-wider trans'>
                                 {profile?.fullname}
                             </h3>
                             <h6 className='capitalize mt-2 text-clr-3 text-sm'>
@@ -67,7 +79,9 @@ const Profile: React.FC<{ profile: any }> = ({ profile }) => {
                     <div className="profile-card-info">
                         <p className="text-clr-3">Username</p>
                         <p className="text-clr-2">{profile?.user}</p>
-                        {isRole && pathName !== "/staff/profile" ? "" :  <button className="profile-edit-btn">
+                        {isRole && pathName !== "/staff/profile" ? "" : 
+                        <button className="profile-edit-btn"
+                        onClick={() => dispatch({ type: "USERNAME", toggle: true })}>
                             Edit Username
                         </button> }
                     </div>
@@ -86,7 +100,8 @@ const Profile: React.FC<{ profile: any }> = ({ profile }) => {
                             <p className="text-clr-2 capitalize">
                                 {authRoles.join(", ")}
                             </p>
-                            <button className="profile-edit-btn">
+                            <button className="profile-edit-btn"
+                            onClick={() => dispatch({ type: "ROLES", toggle: true })}>
                                 Edit Roles
                             </button>
                         </div>}
@@ -95,7 +110,8 @@ const Profile: React.FC<{ profile: any }> = ({ profile }) => {
                             <p title={`${profile?.resigned?.resign ? `Resigned on ${convertISODate(profile?.resigned.date)}` : "Staff hasn't resigned."}`} >
                                 {profile?.resigned?.resign ? "Resigned": "Null"}
                             </p>
-                            {isRole && <button className="profile-edit-btn">
+                            {isRole && <button className="profile-edit-btn"
+                            onClick={() => dispatch({ type: "RESIG", toggle: true })}>
                                 Edit Resignation
                             </button>}
                         </div>
