32637cd6d969b134fa6605748ffafbb162be5219
Raheem Kawojue <kawojue08@gmail.com>
2023-06-10 13:51:09 +0100

    [add] more reducers

--- client/utils/modalReducers.ts
+++ client/utils/modalReducers.ts
@@ -1,24 +1,22 @@
 const modalReducer = (state: ModalStates, action: ModalActions) => {
     switch(action.type) {
         case "RESIG": {
-            return {
-                ...state, resignation: action.toggle
-            }
+            return { ...state, resignation: action.toggle }
         }
         case "PSWD": {
-            return {
-                ...state, password: action.toggle
-            }
+            return { ...state, password: action.toggle }
         }
         case "ROLES": {
-            return {
-                ...state, username: action.toggle
-            }
+            return { ...state, roles: action.toggle }
         }
         case "USERNAME": {
-            return {
-                ...state, roles: action.toggle
-            }
+            return { ...state, username: action.toggle }
+        }
+        case "FULLNAME": {
+            return { ...state, fullname: action.toggle }
+        }
+        case "AVATAR": {
+            return { ...state, avatar: action.toggle }
         }
 
         default: {
