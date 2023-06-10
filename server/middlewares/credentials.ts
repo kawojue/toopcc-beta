704dd0cbcd57edca99e809b7722af3d9fb61bab0
Raheem Kawojue <kawojue08@gmail.com>
2023-06-10 11:36:24 +0100

    .

--- server/middlewares/credentials.ts
+++ server/middlewares/credentials.ts
@@ -5,7 +5,7 @@ const credentials = (req: Request, res: any, next: NextFunction) => {
     const origin: unknown = req.headers.origin
     if (allowedOrigins.includes(origin as string)) {
         res.header('Access-Control-Allow-Credentials', true)
-        res.header("Access-Control-Allow-Origin", origin);
+        res.header("Access-Control-Allow-Origin", origin)
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     }
     next()
