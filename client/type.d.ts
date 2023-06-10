d8bb04bbbe5ffa5b478d3207bc68d07a781d3971
Raheem Kawojue <kawojue08@gmail.com>
2023-06-10 12:52:54 +0100

    [add] types and interface

--- client/type.d.ts
+++ client/type.d.ts
@@ -8,3 +8,16 @@ interface IProfile {
         profile: string
     }
 }
+
+interface ModalStates {
+    roles: boolean,
+    username: boolean,
+    password: boolean,
+    resignation: boolean,
+}
+
+type ModalActions =
+| { type: 'RESIG'; toggle: boolean }
+| { type: 'PSWD'; toggle: boolean }
+| { type: 'ROLES'; toggle: boolean }
+| { type: 'USERNAME'; toggle: boolean }
