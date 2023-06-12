ca1c90607f80ae5b528bebabb2e8dbe1036fd1d4
Raheem Kawojue <kawojue08@gmail.com>
2023-06-12 14:53:03 +0100

    [add] icons

--- client/public/icons/ico.ts
+++ client/public/icons/ico.ts
@@ -1,12 +1,13 @@
 import { HiOutlineKey } from 'react-icons/hi'
+import { RiDeleteBin6Line } from 'react-icons/ri'
 import {
-    AiFillEyeInvisible, AiFillEye,
-    AiOutlineCamera, AiOutlineMail
+    AiFillEyeInvisible, AiOutlineCloudUpload,
+    AiOutlineCamera, AiOutlineMail, AiFillEye
 } from 'react-icons/ai'
 import { FaTimes } from 'react-icons/fa'
 
 
 export {
     AiFillEyeInvisible, AiFillEye, AiOutlineCamera, AiOutlineMail,
-    HiOutlineKey, FaTimes
+    HiOutlineKey, FaTimes, AiOutlineCloudUpload, RiDeleteBin6Line
 }
