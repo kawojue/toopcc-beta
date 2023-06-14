2f5a316d5cdb01b36e6bee97c0393286f0752930
Raheem Kawojue <kawojue08@gmail.com>
2023-06-14 14:45:53 +0100

    ..

--- server/dist/routes/root.js
+++ server/dist/routes/root.js
@@ -10,7 +10,7 @@ const getPatient_1 = __importDefault(require("./api/getPatient"));
 const express_1 = require("express");
 const root = (0, express_1.Router)();
 root.use('/auth', auth_1.default);
-root.use('/api/user', user_1.default);
+root.use('/api/users', user_1.default);
 root.use('/patients', setPatient_1.default);
 root.use('/api/patients', getPatient_1.default);
 root.get('/', (req, res) => {
