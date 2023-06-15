3afc9daf03248c2d7c7bb2e6a681b888c49c8a66
Raheem Kawojue <kawojue08@gmail.com>
2023-06-15 09:47:19 +0100

    [add] pt reducer func

---
+++ client/utils/patientReducer.ts
@@ -0,0 +1,31 @@
+const patientReducer = (state: PatientStates, action: PatientActions) => {
+    switch (action.type) {
+        case 'FULLN': {
+            return { ...state, fullname: action.payload }
+        }
+        case 'SEX': {
+            return { ...state, sex: action.payload }
+        }
+        case 'AGE': {
+            return { ...state, age: action.payload }
+        }
+        case 'ADDR': {
+            return { ...state, address: action.payload }
+        }
+        case 'PHN': {
+            return { ...state, phone_no: action.payload }
+        }
+        case 'CARD_NO': {
+            return { ...state, card_no: action.payload }
+        }
+        case 'CARDNO': {
+            return { ...state, cardNo: action.payload }
+        }
+    
+        default: {
+            return state
+        }
+    }
+}
+
+export default patientReducer
