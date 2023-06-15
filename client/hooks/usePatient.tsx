a6021cc00a33ddf2840072b999249980e9428eba
Raheem Kawojue <kawojue08@gmail.com>
2023-06-15 09:48:06 +0100

    [add] pt states

--- client/hooks/usePatient.tsx
+++ client/hooks/usePatient.tsx
@@ -1,11 +1,31 @@
 "use client"
 import useAuth from './useAuth'
-import { createContext, useContext, useEffect, useState, useRef } from 'react'
+import {
+    createContext, useState, useRef,
+    useContext, useEffect, useReducer
+} from 'react'
+import patientReducer from '@/utils/patientReducer'
 
 const Patient = createContext({})
 
+const initialStates: PatientStates = {
+    sex: '',
+    age: '',
+    date: '',
+    cardNo: '',
+    card_no: '',
+    address: '',
+    fullname: '',
+    phone_no: '',
+    death: {
+        dead: false,
+        date: ''
+    },
+}
+
 export const PatientProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const { token, auth }: any = useAuth()
+    const [state, dispatch]= useReducer(patientReducer, initialStates)
 
     return (
         <Patient.Provider value={{
