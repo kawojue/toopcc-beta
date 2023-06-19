5492a6f92655af7d36981154c4dc34b76f04feb6
Raheem Kawojue <kawojue08@gmail.com>
2023-06-19 10:34:03 +0100

    on it..

--- client/components/Patients.tsx
+++ client/components/Patients.tsx
@@ -1,13 +1,21 @@
 "use client"
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const Patients = ({ patients }: any) => {
-    return (
-        <section>
-            {patients.map((patient: any) => {
-                <article key={patient.card_no} >
+    const router = useRouter()
 
+    return (
+        <section className="flex flex-col gap-2">
+            {patients.map((patient: any) => (
+                <article key={patient._id} className="bg-clr-5">
+                    <Link href={`/patients/${patient.card_no}`} className="flex justify-between items-center">
+                        <p>{patient.card_no}</p>
+                        <p>{patient.fullname}</p>
+                        <p>{patient.sex}</p>
+                    </Link>
                 </article>
-            })}
+            ))}
         </section>
     )
 }
