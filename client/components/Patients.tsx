892f1b1f612c4e6077748b7c82833ef8a1e6c55b
Raheem Kawojue <kawojue08@gmail.com>
2023-06-20 11:21:20 +0100

    [edit] routing

--- client/components/Patients.tsx
+++ client/components/Patients.tsx
@@ -9,7 +9,8 @@ const Patients = ({ patients }: any) => {
         <section className="flex flex-col gap-2">
             {patients.map((patient: any) => (
                 <article key={patient._id} className="bg-clr-5">
-                    <Link href={`/patients/${patient.card_no}`} className="flex justify-between items-center">
+                    <Link href={`/patients/${patient.card_no?.split('/')[0]}`}
+                    className="flex justify-between items-center">
                         <p>{patient.card_no}</p>
                         <p>{patient.fullname}</p>
                         <p>{patient.sex}</p>
