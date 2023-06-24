f2274363e697c38a05eef75ad2e05dc3874cb1ee
Raheem Kawojue <kawojue08@gmail.com>
2023-06-24 01:00:27 +0100

    [add] handle pt func

--- client/hooks/usePatient.tsx
+++ client/hooks/usePatient.tsx
@@ -1,9 +1,11 @@
 "use client"
 import useAuth from './useAuth'
+import axios from '@/app/api/instance'
 import {
     createContext, useState, useRef,
     useContext, useEffect, useReducer
 } from 'react'
+import throwError from '@/utils/throwError'
 import patientReducer from '@/utils/patientReducer'
 
 const Patient = createContext({})
@@ -23,11 +25,26 @@ const initialStates: PatientStates = {
 
 export const PatientProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const { token, auth }: any = useAuth()
+    const [patient, setPatient] = useState({})
+    const [profLoad, setProfLoad] = useState<boolean>(false)
     const [state, dispatch]= useReducer(patientReducer, initialStates)
+
+    const handlePatient = async (card_no: string) => {
+        setProfLoad(true)
+        await axios.get(
+            `/api/patients/patient/${card_no}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+        ).then((res: any) => setPatient(res.data?.patient))
+        .catch((err: any) => throwError(err)).finally(() => setProfLoad(false))
+    }
 
     return (
         <Patient.Provider value={{
-            token, auth, state, dispatch
+            token, auth, state, dispatch, handlePatient, patient, profLoad
         }}>
             {children}
         </Patient.Provider>
