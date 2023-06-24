75afe40c9c0db675a0d432c31b39e1c1966edd56
Raheem Kawojue <kawojue08@gmail.com>
2023-06-24 01:01:50 +0100

    [add] ptId page

---
+++ client/app/patients/[patientId]/page.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import { useEffect } from 'react'
+import usePatient from "@/hooks/usePatient"
+import { SpinnerTwo } from '@/components/Spinner'
+
+const page = ({ params: { patientId } }: { params: { patientId: string } }) => {
+    const { handlePatient, patient, profLoad, token }: any = usePatient()
+
+    useEffect(() => {
+        if (token) (async () => await handlePatient(patientId))()
+    }, [token])
+
+    if (profLoad) return <SpinnerTwo />
+
+    console.log(patient)
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default page
