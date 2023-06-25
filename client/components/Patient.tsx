93ac9f5050ad30a31232087098a5970d263d2eaf
Raheem Kawojue <kawojue08@gmail.com>
2023-06-25 01:50:11 +0100

    [edit] structure

--- client/components/Patient.tsx
+++ client/components/Patient.tsx
@@ -1,10 +1,46 @@
 "use client"
+import convertISODate from "@/utils/shortDate"
 
 const Patient = ({ patient }: any) => {
+    console.log(patient)
+
     return (
-        <>
-            {JSON.stringify(patient)}
-        </>
+        <section>
+            <article className="relative">
+                <div>
+                    <span>Card Number: </span>
+                    <span>{patient.card_no}</span>
+                </div>
+                <div>
+                    <div>
+                        <span>Full name: </span>
+                        <span>{patient.fullname}</span>
+                    </div>
+                    <div>
+                        <span>Address: </span>
+                        <span>{patient.address || 'Null'}</span>
+                    </div>
+                    <div>
+                        <span>Date: </span>
+                        <span>{convertISODate(patient.date) || 'Null'}</span>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <span>Age: </span>
+                        <span>{patient.age}</span>
+                    </div>
+                    <div>
+                        <span>Sex: </span>
+                        <span>{patient.sex}</span>
+                    </div>
+                    <div>
+                        <span>Phone Number: </span>
+                        <span>{patient.phone_no || 'Null'}</span>
+                    </div>
+                </div>
+            </article>
+        </section>
     )
 }
 
