daf1ee705731700fb641205ef0e458fa0c6c245f
Raheem Kawojue <kawojue08@gmail.com>
2023-06-25 01:40:26 +0100

    [add] card no formatter

---
+++ client/utils/formatCardNo.ts
@@ -0,0 +1,18 @@
+const formatCardNo = (patient: any) => {
+    if (!patient) return
+    
+    const { card_no: cardNo, date: ISOStringDate } = patient
+    let formattedDate: any
+    if (ISOStringDate) {
+        const date = new Date(ISOStringDate)
+        const year = date.getFullYear()
+        formattedDate = `/${String(year).slice(2)}`
+    } else {
+        formattedDate = ""
+    }
+
+    const card_no = cardNo + formattedDate
+    return { ...patient, card_no }
+}
+
+export default formatCardNo
