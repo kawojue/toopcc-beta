91a76976701fdc1940bea810d4c1cf527750b277
Raheem Kawojue <kawojue08@gmail.com>
2023-06-27 00:07:04 +0100

    [edit] refractored

--- client/components/Patient.tsx
+++ client/components/Patient.tsx
@@ -1,41 +1,45 @@
 "use client"
+import notify from "@/utils/notify"
 import convertISODate from "@/utils/shortDate"
 
 const Patient = ({ patient }:any) => {
     console.log(patient)
 
+
     return (
         <section className="patients md:max-w-[800px] overflow-x-hidden">
-            <div>
-                <span>Card Number:</span>
-                <span>{patient.card_no}</span>
+            <div className="patient_info absolute top-0 right-5">
+                <span>Card No:</span>
+                <span >
+                    {patient.card_no}
+                </span>
             </div>
-            <article className="w-full flex justify-between">
-                <div className="flex flex-col gap-1.5">
-                    <div>
-                        <span>Full name:</span>
+            <article className="flex flex-col justify-between gap-5 mt-9 md:items-center md:flex-row">
+                <div className="patient_info flex-col">
+                    <div className="patient_info md:text-[19px]">
+                        <span>Full name: </span>
                         <span>{patient.fullname}</span>
                     </div>
-                    <div>
+                    <div className="patient_info md:text-lg">
                         <span>Address:</span>
                         <span>{patient.address || 'Null'}</span>
                     </div>
-                    <div>
+                    <div className="patient_info">
                         <span>Date:</span>
                         <span>{convertISODate(patient.date) || 'Null'}</span>
                     </div>
                 </div>
                 <div className="flex flex-col gap-1.5">
-                    <div>
+                    <div className="patient_info">
                         <span>Age:</span>
                         <span>{patient.age}</span>
                     </div>
-                    <div>
+                    <div className="patient_info">
                         <span>Sex:</span>
                         <span>{patient.sex}</span>
                     </div>
-                    <div>
-                        <span>Phone Number:</span>
+                    <div className="patient_info">
+                        <span>Phone No:</span>
                         <span>{patient.phone_no || 'Null'}</span>
                     </div>
                 </div>
