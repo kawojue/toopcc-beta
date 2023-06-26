382ed7034da51945ccfb3dda92d0e69d8a658df9
Raheem Kawojue <kawojue08@gmail.com>
2023-06-26 01:10:41 +0100

    [edit] structure

--- client/components/Patient.tsx
+++ client/components/Patient.tsx
@@ -1,41 +1,41 @@
 "use client"
 import convertISODate from "@/utils/shortDate"
 
-const Patient = ({ patient }: any) => {
+const Patient = ({ patient }:any) => {
     console.log(patient)
 
     return (
-        <section>
-            <article className="relative">
-                <div>
-                    <span>Card Number: </span>
-                    <span>{patient.card_no}</span>
-                </div>
-                <div>
+        <section className="patients md:max-w-[800px] overflow-x-hidden">
+            <div>
+                <span>Card Number:</span>
+                <span>{patient.card_no}</span>
+            </div>
+            <article className="w-full flex justify-between">
+                <div className="flex flex-col gap-1.5">
                     <div>
-                        <span>Full name: </span>
+                        <span>Full name:</span>
                         <span>{patient.fullname}</span>
                     </div>
                     <div>
-                        <span>Address: </span>
+                        <span>Address:</span>
                         <span>{patient.address || 'Null'}</span>
                     </div>
                     <div>
-                        <span>Date: </span>
+                        <span>Date:</span>
                         <span>{convertISODate(patient.date) || 'Null'}</span>
                     </div>
                 </div>
-                <div>
+                <div className="flex flex-col gap-1.5">
                     <div>
-                        <span>Age: </span>
+                        <span>Age:</span>
                         <span>{patient.age}</span>
                     </div>
                     <div>
-                        <span>Sex: </span>
+                        <span>Sex:</span>
                         <span>{patient.sex}</span>
                     </div>
                     <div>
-                        <span>Phone Number: </span>
+                        <span>Phone Number:</span>
                         <span>{patient.phone_no || 'Null'}</span>
                     </div>
                 </div>
