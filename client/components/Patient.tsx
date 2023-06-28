3cc636b071688a866343aec1ef66613c538c6f4b
Raheem Kawojue <kawojue08@gmail.com>
2023-06-28 09:46:31 +0100

    ..

--- client/components/Patient.tsx
+++ client/components/Patient.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { lato } from "@/public/font/font"
 import convertISODate from "@/utils/shortDate"
 
 const Patient = ({ patient }:any) => {
@@ -7,37 +8,37 @@ const Patient = ({ patient }:any) => {
     return (
         <section className="patients md:max-w-[800px] overflow-x-hidden">
             <div className="patient_info tracking-wider absolute top-0 right-5">
-                <span className="font-bold">Card No:</span>
+                <span className="font-bold">Card No :</span>
                 <span className="font-semibold">
                     {patient.card_no}
                 </span>
             </div>
-            <article className="flex flex-col justify-between gap-5 mt-9 md:items-center md:flex-row">
+            <article className={`flex flex-col justify-between gap-5 mt-9 md:items-center md:flex-row`}>
                 <div className="patient_info flex-col">
-                    <div className="patient_info md:text-[19px]">
-                        <span>Full name: </span>
+                    <div className="patient_info md:text-[17px]">
+                        <span>Full name :</span>
                         <span>{patient.fullname}</span>
                     </div>
-                    <div className="patient_info">
-                        <span>Address:</span>
+                    <div className="patient_info md:text-[17px]">
+                        <span>Address :</span>
                         <span>{patient.address || 'Null'}</span>
                     </div>
-                    <div className="patient_info">
-                        <span>Date:</span>
+                    <div className="patient_info md:text-[17px]">
+                        <span>Date :</span>
                         <span>{convertISODate(patient.date) || 'Null'}</span>
                     </div>
                 </div>
                 <div className="flex flex-col gap-1.5">
-                    <div className="patient_info">
-                        <span>Age:</span>
+                    <div className="patient_info md:text-[17px]">
+                        <span>Age :</span>
                         <span>{patient.age}</span>
                     </div>
-                    <div className="patient_info">
-                        <span>Sex:</span>
+                    <div className="patient_info md:text-[17px]">
+                        <span>Sex :</span>
                         <span>{patient.sex}</span>
                     </div>
-                    <div className="patient_info">
-                        <span>Phone No:</span>
+                    <div className="patient_info md:text-[17px]">
+                        <span>Phone No :</span>
                         <span>{patient.phone_no || 'Null'}</span>
                     </div>
                 </div>
