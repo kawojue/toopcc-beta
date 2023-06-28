8873022151fa5906a4f28402b0642e4486c5fe3d
Raheem Kawojue <kawojue08@gmail.com>
2023-06-29 00:48:42 +0100

    [edit] refractored

--- client/app/patients/[patientId]/page.tsx
+++ client/app/patients/[patientId]/page.tsx
@@ -2,12 +2,14 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
 import { useEffect } from 'react'
+import useToken from '@/hooks/useToken'
 import Patient from '@/components/Patient'
 import usePatient from '@/hooks/usePatient'
 import { SpinnerTwo } from '@/components/Spinner'
 
 const page = ({ params: { patientId } }: IPt) => {
-    const { handlePatient, patient, profLoad, token }: any = usePatient()
+    const token: string = useToken()
+    const { handlePatient, patient, profLoad }: any = usePatient()
 
     useEffect(() => {
         if (token) (async () => await handlePatient(patientId))()
