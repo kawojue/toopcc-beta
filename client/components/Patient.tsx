097cf4d8b0386e7a2fec7b0397b3290b93b7f7dc
Raheem Kawojue <kawojue08@gmail.com>
2023-07-02 01:05:58 +0100

    [add] patient status

--- client/components/Patient.tsx
+++ client/components/Patient.tsx
@@ -1,19 +1,25 @@
 "use client"
-import { lato } from "@/public/font/font"
 import convertISODate from "@/utils/shortDate"
 
 const Patient = ({ patient }:any) => {
-    console.log(patient)
 
     return (
         <section className="patients md:max-w-[800px] overflow-x-hidden">
-            <div className="patient_info tracking-wider absolute top-0 right-5">
-                <span className="font-bold">Card No :</span>
-                <span className="font-semibold">
-                    {patient.card_no}
-                </span>
-            </div>
-            <article className={`flex flex-col justify-between gap-5 mt-9 md:items-center md:flex-row`}>
+            <article className="w-full flex justify-between">
+                <div className="patient_info tracking-wider ">
+                    <span className="font-bold">Status :</span>
+                    <span className="font-semibold">
+                        {patient.death?.dead === true ? 'Dead': 'Active'}
+                    </span>
+                </div>
+                <div className="patient_info tracking-wider ">
+                    <span className="font-bold">Card No :</span>
+                    <span className="font-semibold">
+                        {patient.card_no}
+                    </span>
+                </div>
+            </article>
+            <article className={`flex flex-col justify-between gap-5 mt-5 md:items-center md:flex-row`}>
                 <div className="patient_info flex-col">
                     <div className="patient_info">
                         <span>Full name :</span>
