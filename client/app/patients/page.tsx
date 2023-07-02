e9a18537aa91ffb22a5970573bd0a68a63bc21f6
Raheem Kawojue <kawojue08@gmail.com>
2023-07-03 00:05:52 +0100

    [edit] refractored

--- client/app/patients/page.tsx
+++ client/app/patients/page.tsx
@@ -1,40 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import axios from '@/app/api/instance'
+import { useEffect } from 'react'
 import useToken from '@/hooks/useToken'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import throwError from '@/utils/throwError'
+import usePatient from '@/hooks/usePatient'
 import Patients from '@/components/Patients'
-import formatCardNo from '@/utils/formatCardNo'
 import { SpinnerTwo } from '@/components/Spinner'
 
 const page = () => {
-    const router = useRouter()
     const token: string = useToken()
-    const [patients, setPatients] = useState<any[]>([])
-    const [loading, setLoading] = useState<boolean>(false)
-
-    const getAllPatients = async (token: string) => {
-        setLoading(true)
-        await axios.get(`/api/patients`, {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        }).then((res: any) => {
-            const pts: any[] = res.data?.patients || []
-            const formattedPatients = pts.map((pt: any) => {
-                return formatCardNo(pt)
-            })
-            setPatients(formattedPatients)
-        }).catch((err: any) => {
-            throwError(err)
-            setTimeout(() => {
-                router.push('/staff/login')
-            }, 500)
-        }).finally(() => setLoading(false))
-    }
+    const {
+        loading, getAllPatients, patients
+    }: any = usePatient()
 
     useEffect(() => {
         if (token) (async () => await getAllPatients(token))()
