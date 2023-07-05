8c6292328b66ec60cd140f1a8869cb365d2c572e
Raheem Kawojue <kawojue08@gmail.com>
2023-07-05 12:58:55 +0100

    ..

--- client/app/patients/[patientId]/diagnosis/page.tsx
+++ client/app/patients/[patientId]/diagnosis/page.tsx
@@ -1,40 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import React, { useRef, useState } from 'react';
 
 const page = () => {
-    const editorRef = useRef<HTMLDivElement>(null);
-    const [content, setContent] = useState<string>('');
-
-    const handleBold = () => {
-        document.execCommand('bold', false);
-    };
-
-    const handleSave = () => {
-        if (!editorRef.current) return
-
-        setContent(editorRef.current.innerHTML);
-    };
-
     return (
-        <>
-        <div className="flex gap-3">
-            <button onClick={handleBold}>Bold</button>
-            <button onClick={() => document.execCommand('italic', false)}>Italic</button>
-            <button onClick={handleSave}>Save</button>
-        </div>
+        <></>
+    )
+}
 
-        <div
-            ref={editorRef}
-            contentEditable="true"
-            style={{ border: '1px solid #ccc', minHeight: '100px', padding: '10px' }}
-        />
-        {/* <div>
-            <h3>Rendered Content:</h3>
-            <div dangerouslySetInnerHTML={{ __html: content }} />
-        </div> */}
-        </>
-    );
-};
-
-export default page;
+export default page
