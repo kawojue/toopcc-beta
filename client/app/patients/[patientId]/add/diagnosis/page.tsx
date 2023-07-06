04fdc2847a840231b6fd2b5e53931cb81645fb63
Raheem Kawojue <kawojue08@gmail.com>
2023-07-06 11:45:08 +0100

    ..

--- client/app/patients/[patientId]/add/diagnosis/page.tsx
+++ client/app/patients/[patientId]/add/diagnosis/page.tsx
@@ -1,35 +1,35 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react'
 
 const page = () => {
-    const editorRef = useRef<HTMLDivElement>(null);
-    const [content, setContent] = useState<string>('');
+    const editorRef = useRef<HTMLDivElement>(null)
+    const [content, setContent] = useState<string>('')
 
     const handleSave = () => {
         if (!editorRef.current) return
 
-        setContent(editorRef.current.innerHTML);
-    };
+        setContent(editorRef.current.innerHTML)
+    }
 
     return (
         <section>
-        <div className="flex gap-3">
-            <button onClick={() => document.execCommand('bold', false)}>Bold</button>
-            <button onClick={() => document.execCommand('italic', false)}>Italic</button>
-            <button onClick={handleSave}>Save</button>
-        </div>
+            <div className="flex gap-3">
+                <button onClick={() => document.execCommand('bold', false)}>Bold</button>
+                <button onClick={() => document.execCommand('italic', false)}>Italic</button>
+                <button onClick={handleSave}>Save</button>
+            </div>
 
-        <div
-            ref={editorRef}
-            contentEditable="true"
-            className=""
-        />
-        {/* <div>
+            <div
+                ref={editorRef}
+                contentEditable="true"
+                className=""
+            />
+            {/* <div>
             <div dangerouslySetInnerHTML={{ __html: content }} />
         </div> */}
         </section>
-    );
-};
+    )
+}
 
-export default page;
+export default page
