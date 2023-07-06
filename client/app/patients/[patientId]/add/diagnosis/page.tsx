34716db4ba0dfe6e794cf950e6f9eb26488b5bff
Raheem Kawojue <kawojue08@gmail.com>
2023-07-06 13:25:17 +0100

    refractored

--- client/app/patients/[patientId]/add/diagnosis/page.tsx
+++ client/app/patients/[patientId]/add/diagnosis/page.tsx
@@ -1,34 +1,25 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import React, { useRef, useState } from 'react'
+import { useState, FormEvent } from 'react'
+import TextEditor from '@/components/TextEditor'
 
 const page = () => {
-    const editorRef = useRef<HTMLDivElement>(null)
-    const [content, setContent] = useState<string>('')
+    const [texts, setTexts] = useState<string>("")
 
-    const handleSave = () => {
-        if (!editorRef.current) return
+    console.log(texts)
 
-        setContent(editorRef.current.innerHTML)
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
     }
 
     return (
-        <section>
-            <div className="flex gap-3">
-                <button onClick={() => document.execCommand('bold', false)}>Bold</button>
-                <button onClick={() => document.execCommand('italic', false)}>Italic</button>
-                <button onClick={handleSave}>Save</button>
-            </div>
-
-            <div
-                ref={editorRef}
-                contentEditable="true"
-                className=""
-            />
-            {/* <div>
-            <div dangerouslySetInnerHTML={{ __html: content }} />
-        </div> */}
-        </section>
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <button onClick={handleSubmit}>
+                Submit
+            </button>
+            <TextEditor textsFn={setTexts} />
+        </form>
     )
 }
 
