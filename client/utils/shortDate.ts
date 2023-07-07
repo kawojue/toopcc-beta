ad409c5963861302dae36b3862bdbaf9faa60ec2
Raheem Muyiwa <kawojue08@gmail.com>
2023-07-07 01:22:57 +0100

    Update shortDate.ts

--- client/utils/shortDate.ts
+++ client/utils/shortDate.ts
@@ -1,12 +1,12 @@
 import { format, parseISO } from "date-fns"
 
 const convertISODate = (ISODate: string): string => {
-    let shortDate: string = ""
+    let shortDate: string = ''
     try {
         const createdAt: Date = parseISO(ISODate)
         shortDate = format(createdAt, "dd/MM/yyyy")
     } catch {
-        shortDate = ""
+        shortDate = ''
     }
 
     return shortDate
