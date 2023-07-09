da860ce9fe5c41d4a2b0389e36f3e7e123ef7a2f
Raheem Muyiwa <kawojue08@gmail.com>
2023-07-10 00:04:40 +0100

    Update checkRoles.ts

--- server/utilities/checkRoles.ts
+++ server/utilities/checkRoles.ts
@@ -1,7 +1,5 @@
 export default function checkRoles(authRoles: string[], roles: string[]): boolean {
-    if (!authRoles || !roles) {
-        return false
-    }
+    if (!authRoles || !roles) return false
 
     const result: any = roles.map((role: string) => authRoles.includes(role)).find((value: boolean) => value === true)
     if (!result) {
