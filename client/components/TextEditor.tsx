7ddea4c654658bcab9a6106e95db0f3215893edf
Raheem Kawojue <kawojue08@gmail.com>
2023-07-12 00:02:54 +0100

    [use] icons

--- client/components/TextEditor.tsx
+++ client/components/TextEditor.tsx
@@ -1,23 +1,33 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
+import {
+    FaItalic, FaBold,
+    FaUnderline, FaCopy,
+} from '@/public/icons/ico'
 
 const TextEditor: React.FC<{ textEditorRef: any }> = ({ textEditorRef }) => {
-
     return (
         <section>
-            <div className="flex gap-3">
-                <button type="button" onClick={() => document.execCommand('bold', false)}>
-                    Bold
+            <div>
+                <button type="button" title='Bold' className="editor-btn"
+                    onClick={() => document.execCommand('bold', false)}>
+                    <FaBold />
                 </button>
-                <button type="button" onClick={() => document.execCommand('italic', false)}>
-                    Italic
+                <button type="button" title='Italic' className="editor-btn"
+                    onClick={() => document.execCommand('italic', false)}>
+                    <FaItalic />
                 </button>
-                <button type="button" onClick={() => document.execCommand('underline', false)}>
-                    Underline
+                <button type="button" title='Underline' className="editor-btn"
+                    onClick={() => document.execCommand('underline', false)}>
+                    <FaUnderline />
+                </button>
+                <button type="button" title='Copy' className="editor-btn"
+                    onClick={() => document.execCommand('copy', false)}>
+                    <FaCopy />
                 </button>
             </div>
             <div ref={textEditorRef} contentEditable="true"
-                style={{ border: '1px solid #ccc', minHeight: '100px', padding: '10px' }} />
+                style={{ border: '1px solid #ccc', minHeight: '100px', padding: '10px', resize: 'both' }} />
         </section>
     )
 }
