e7291da0a3a40607d3cbefdaf8b0aed08a9d6912
Raheem Kawojue <kawojue08@gmail.com>
2023-07-12 00:02:15 +0100

    on file upload

--- client/components/PicsUpload.tsx
+++ client/components/PicsUpload.tsx
@@ -1,8 +1,24 @@
 "use client"
+import { handleFile } from "@/utils/file"
 
 const PicsUpload: React.FC = () => {
 
     return (
-        <></>
+        <section>
+            <button>
+                <label></label>
+                <input type="file" />
+            </button>
+            <button>
+                <label></label>
+                <input type="file" />
+            </button>
+            <button>
+                <label></label>
+                <input type="file" />
+            </button>
+        </section>
     )
 }
+
+export default PicsUpload
