6cfbdfd8a14a5ee516a494f72366aeed9fe72743
Raheem Kawojue <kawojue08@gmail.com>
2023-07-13 00:10:40 +0100

    more on pics

--- client/app/patients/[patientId]/add/diagnosis/page.tsx
+++ client/app/patients/[patientId]/add/diagnosis/page.tsx
@@ -1,22 +1,33 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
+import useToken from '@/hooks/useToken'
+import usePatient from '@/hooks/usePatient'
 import PicsUpload from '@/components/PicsUpload'
 import TextEditor from '@/components/TextEditor'
-import { useState, FormEvent, useRef } from 'react'
+import { useState, FormEvent, useRef, useEffect } from 'react'
 
 const page = () => {
+    const token: string = useToken()
+    const { pic1, pic2, pic3 } = usePatient()
     const textEditorRef = useRef<HTMLDivElement>(null)
+    const [picsArray, setPicsArray] = useState<string[]>([])
 
-    const handleSubmit = (e: FormEvent) => {
+    useEffect(() => {
+        const array: string[] = [pic1, pic2, pic3].filter((arr: string) => arr !== '')
+        setPicsArray(array)
+    }, [pic1, pic2, pic3])
+
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
         if (!textEditorRef.current) return
 
+        console.log(picsArray.length)
         console.log(textEditorRef.current.innerHTML)
     }
 
     return (
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form onSubmit={async (e) => await handleSubmit(e)}>
             <button onClick={handleSubmit}>
                 Submit
             </button>
