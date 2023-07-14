b237cb46c8b09b28b683686f91c8063a67139037
Raheem Kawojue <kawojue08@gmail.com>
2023-07-14 21:11:46 +0100

    refractored

--- client/app/staff/login/page.tsx
+++ client/app/staff/login/page.tsx
@@ -1,21 +1,21 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
 import Link from "next/link"
+import { useEffect } from "react"
 import useAuth from "@/hooks/useAuth"
 import { inter } from "@/public/font/font"
-import { useState, useEffect } from 'react'
 import { useRouter } from "next/navigation"
+import { useAuthStore } from "@/utils/store"
 import PswdButton from "@/components/PswdBtn"
 import SubmitBtn from "@/components/SubmitBtn"
 
 const page = () => {
     const router = useRouter()
     const {
-        pswd, handleLogin, auth,
-        userId, setUserId, setPswd,
-    }: any = useAuth()
-
-    const [pswdBtn, setPswdBtn] = useState<boolean>(false)
+        pswd, auth, userId, setPswd,
+        setUserId, pswdBtn, setPswdBtn
+    } = useAuthStore()
+    const { handleLogin }: any = useAuth()
 
     useEffect(() => {
         if (auth) router.push('/staff/profile')
@@ -25,18 +25,18 @@ const page = () => {
         <section className="section-form">
             <h2 className="form-header">Login</h2>
             <form onSubmit={e => e.preventDefault()}
-            className="form">
+                className="form">
                 <article className="form-group">
                     <label htmlFor="userId">Email or Username</label>
                     <input type="text" id="userId" className={inter.className}
-                    value={userId} onChange={(e) => setUserId(e.target.value)}/>
+                        value={userId} onChange={(e) => setUserId(e.target.value)} />
                 </article>
                 <article className="form-group">
                     <label htmlFor="pswd">Password</label>
                     <div className="pswd-container">
-                        <input type={`${pswdBtn ? 'text': 'password'}`} id="pswd"
-                        value={pswd} onChange={(e) => setPswd(e.target.value)}
-                        className={inter.className} placeholder="password" />
+                        <input type={`${pswdBtn ? 'text' : 'password'}`} id="pswd"
+                            value={pswd} onChange={(e) => setPswd(e.target.value)}
+                            className={inter.className} placeholder="password" />
                         <PswdButton get={pswdBtn} set={setPswdBtn} />
                     </div>
                     <Link href="/staff/password/verify" className="fgt-pswd">
