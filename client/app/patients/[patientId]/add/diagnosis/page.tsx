a4b7403ec14b5562b9aea6fd21263b3981bc8f86
Raheem Kawojue <kawojue08@gmail.com>
2023-07-14 14:51:25 +0100

    refractored

--- client/app/patients/[patientId]/add/diagnosis/page.tsx
+++ client/app/patients/[patientId]/add/diagnosis/page.tsx
@@ -1,38 +1,77 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
+import notify from '@/utils/notify'
+import { parseISO } from 'date-fns'
+import axios from '@/app/api/instance'
 import useToken from '@/hooks/useToken'
-import usePatient from '@/hooks/usePatient'
-import PicsUpload from '@/components/PicsUpload'
+import throwError from '@/utils/throwError'
 import TextEditor from '@/components/TextEditor'
-import { useState, FormEvent, useRef, useEffect } from 'react'
+import PhotoUpload from '@/components/PhotoUpload'
+import { FormEvent, useRef, useEffect } from 'react'
+import { useDiagnosis, usePhoto } from '@/utils/store'
 
-const page = () => {
+const page = ({ params: { patientId } }: IPt) => {
     const token: string = useToken()
-    const { pic1, pic2, pic3 } = usePatient()
+    const { photo1, photo2, photo3 } = usePhoto()
+    const {
+        isLoading, setIsLoading,
+        photoArray, setPhotoArray,
+        currentDate, setCurrentDate,
+        nextAppDate, setNextAppDate
+    } = useDiagnosis()
     const textEditorRef = useRef<HTMLDivElement>(null)
-    const [picsArray, setPicsArray] = useState<string[]>([])
 
     useEffect(() => {
-        const array: string[] = [pic1, pic2, pic3].filter((arr: string) => arr !== '')
-        setPicsArray(array)
-    }, [pic1, pic2, pic3])
+        const array: string[] = [photo1, photo2, photo3].filter((arr: string) => arr !== '')
+        setPhotoArray(array)
+    }, [photo1, photo2, photo3])
+
+    function formatDate(date: string): string {
+        return date ? parseISO(date).toISOString() : ''
+    }
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
+
+        setIsLoading(true)
         if (!textEditorRef.current) return
 
-        console.log(picsArray.length)
+        console.log(photoArray.length)
         console.log(textEditorRef.current.innerHTML)
+
+        await axios.post(
+            `/patients/diagnosis/${patientId}`,
+            JSON.stringify({
+                images: photoArray, texts: textEditorRef.current.innerHTML,
+                next_app: formatDate(nextAppDate), date: formatDate(currentDate)
+            }), {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then((res: any) => notify(res.data?.action, res.data?.msg))
+            .catch((err: any) => throwError(err))
+            .finally(() => setIsLoading(false))
     }
 
     return (
         <form onSubmit={async (e) => await handleSubmit(e)}>
-            <button onClick={handleSubmit}>
+            <button type='submit' onClick={handleSubmit}>
                 Submit
             </button>
+            <div>
+                <label>Current Date</label>
+                <input value={currentDate} type="date"
+                    onChange={(e) => setCurrentDate(e.target.value)} />
+            </div>
+            <div>
+                <label>Next Appointment Date</label>
+                <input value={nextAppDate} type="date"
+                    onChange={(e) => setNextAppDate(e.target.value)} />
+            </div>
             <TextEditor textEditorRef={textEditorRef} />
-            <PicsUpload />
+            <PhotoUpload />
         </form>
     )
 }
