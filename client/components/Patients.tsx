64ae22f258987928f0233d3ed41eaebe7eb6625c
Raheem Kawojue <kawojue08@gmail.com>
2023-07-14 20:52:55 +0100

    refractored

--- client/components/Patients.tsx
+++ client/components/Patients.tsx
@@ -1,43 +1,43 @@
 "use client"
-import { useState } from 'react'
 import PatientList from './PatientList'
 import { catamaran } from '@/public/font/font'
+import { usePatientStore } from '@/utils/store'
 import { RxMagnifyingGlass } from '@/public/icons/ico'
 
 const Patients = ({ patients }: any) => {
-    const [search, setSeach]= useState<string>("")
+    const { search, setSearch } = usePatientStore()
 
     // search
     const searchQuery: any[] = patients.filter((patient: any) =>
-    (patient.fullname?.toLowerCase()?.includes(search.toLowerCase())) // by name
-    || (patient.address?.toLowerCase()?.includes(search.toLowerCase())) // by address
-    || (patient.card_no?.includes(search.toUpperCase())) // by card number
-    || (patient.phone_no?.includes(search))) // by phone number
+        (patient.fullname?.toLowerCase()?.includes(search.toLowerCase())) // by name
+        || (patient.address?.toLowerCase()?.includes(search.toLowerCase())) // by address
+        || (patient.card_no?.includes(search.toUpperCase())) // by card number
+        || (patient.phone_no?.includes(search))) // by phone number
 
     return (
         <>
-        <section className="patients md:max-w-[800px] overflow-x-hidden">
-            <div className="absolute top-10 right-2">
-                <div className="relative">
-                    <input type="text" value={search} className="w-20 md:w-24 lg:w-28"
-                    onChange={(e) => setSeach(e.target.value)} />
-                    <RxMagnifyingGlass className="absolute top-1.5 right-2.5" />
+            <section className="patients md:max-w-[800px] overflow-x-hidden">
+                <div className="absolute top-10 right-2">
+                    <div className="relative">
+                        <input type="text" value={search} className="w-20 md:w-24 lg:w-28"
+                            onChange={(e) => setSearch(e.target.value)} />
+                        <RxMagnifyingGlass className="absolute top-1.5 right-2.5" />
+                    </div>
                 </div>
-            </div>
-            <div className="patients-header">
-                <h1 className={`${catamaran.className} text-clr-2`}>
-                    List of All Patients
-                </h1>
-                <span></span>
-            </div>
-            {searchQuery.length === 0 ?
-            <p className='text-clr-8 font-semibold text-2xl text-center'>
-                Patient does not exist.
-            </p> : 
-            <>
-                {searchQuery.map((patient: any) => (<PatientList patient={patient} key={patient._id} />))}
-            </>}
-        </section>
+                <div className="patients-header">
+                    <h1 className={`${catamaran.className} text-clr-2`}>
+                        List of All Patients
+                    </h1>
+                    <span></span>
+                </div>
+                {searchQuery.length === 0 ?
+                    <p className='text-clr-8 font-semibold text-2xl text-center'>
+                        Patient does not exist.
+                    </p> :
+                    <>
+                        {searchQuery.map((patient: any) => (<PatientList patient={patient} key={patient._id} />))}
+                    </>}
+            </section>
         </>
     )
 }
