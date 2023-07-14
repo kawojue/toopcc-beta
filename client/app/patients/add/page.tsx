0b15423219ac9a6de7b4c83aecf6418e82bb2928
Raheem Kawojue <kawojue08@gmail.com>
2023-07-14 20:57:24 +0100

    refractored

--- client/app/patients/add/page.tsx
+++ client/app/patients/add/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
+import { FormEvent } from 'react'
 import notify from '@/utils/notify'
 import { parseISO } from 'date-fns'
 import axios from '@/app/api/instance'
@@ -7,13 +8,14 @@ import useToken from '@/hooks/useToken'
 import { lato } from '@/public/font/font'
 import usePatient from '@/hooks/usePatient'
 import throwError from '@/utils/throwError'
-import { useState, FormEvent } from 'react'
+import { useAuthStore } from '@/utils/store'
 import { SpinnerOne } from '@/components/Spinner'
+import { AxiosError, AxiosResponse } from 'axios'
 
 const page = () => {
     const token: string = useToken()
+    const { loading, setLoading } = useAuthStore()
     const { state, dispatch }: IPatient = usePatient()
-    const [loading, setLoading] = useState<boolean>(false)
 
     const handleSubmit = async (e: FormEvent): Promise<void> => {
         e.preventDefault()
@@ -24,17 +26,17 @@ const page = () => {
                 sex: state.sex, age: state.age,
                 phone_no: state.phone_no, address: state.address,
                 card_no: state.card_no, fullname: state.fullname,
-                date: state.date ? parseISO(state.date).toISOString(): "",
+                date: state.date ? parseISO(state.date).toISOString() : "",
             }),
             {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
             }
-        ).then((res: any) => {
+        ).then((res: AxiosResponse) => {
             dispatch({ type: 'RESET' })
             notify(res.data?.action, res.data?.msg)
-        }).catch((err: any) => throwError(err)).finally(() => setLoading(false))
+        }).catch((err: AxiosError) => throwError(err)).finally(() => setLoading(false))
     }
 
     return (
@@ -47,28 +49,28 @@ const page = () => {
                     <div className="flex flex-col gap-1">
                         <label>Card Number</label>
                         <input value={state.card_no} type='text' className="w-24"
-                        onChange={(e) => dispatch({ type: 'CARD_NO', payload: e.target.value })} />
+                            onChange={(e) => dispatch({ type: 'CARD_NO', payload: e.target.value })} />
                     </div>
                     <div className="flex flex-col gap-1">
                         <label>Date</label>
                         <input value={state.date} type="date"
-                        onChange={(e) => dispatch({ type: 'DATE', payload: e.target.value })} />
+                            onChange={(e) => dispatch({ type: 'DATE', payload: e.target.value })} />
                     </div>
                 </article>
                 <article className="flex items-center gap-3">
                     <label>Full name</label>
                     <input value={state.fullname} type='text' className="flex-grow"
-                    onChange={(e) => dispatch({ type: 'FULLN', payload: e.target.value })} />
+                        onChange={(e) => dispatch({ type: 'FULLN', payload: e.target.value })} />
                 </article>
                 <article className="flex items-center gap-3">
                     <label>Address</label>
                     <input value={state.address} type='text' className="flex-grow"
-                    onChange={(e) => dispatch({ type: 'ADDR', payload: e.target.value })} />
+                        onChange={(e) => dispatch({ type: 'ADDR', payload: e.target.value })} />
                 </article>
                 <div className="flex justify-between">
                     <article className="flex gap-1.5 self-end items-center">
-                            <label>Age</label>
-                            <input value={state.age} type='text' className="w-10"
+                        <label>Age</label>
+                        <input value={state.age} type='text' className="w-10"
                             onChange={(e) => dispatch({ type: 'AGE', payload: e.target.value })} />
                     </article>
                     <article>
@@ -76,12 +78,12 @@ const page = () => {
                         <div className="flex gap-3 items-center">
                             <div className="flex gap-1 items-center">
                                 <input value='Male' type="radio" id='male' name="sex"
-                                onChange={(e) => dispatch({ type: "SEX", payload: e.target.value })} />
+                                    onChange={(e) => dispatch({ type: "SEX", payload: e.target.value })} />
                                 <label htmlFor='male'>Male</label>
                             </div>
                             <div className="flex gap-1 items-center">
                                 <input value='Female' type="radio" id='female' name="sex"
-                                onChange={(e) => dispatch({ type: "SEX", payload: e.target.value })} />
+                                    onChange={(e) => dispatch({ type: "SEX", payload: e.target.value })} />
                                 <label htmlFor='female'>Female</label>
                             </div>
                         </div>
@@ -90,11 +92,11 @@ const page = () => {
                 <article className="flex items-center gap-3">
                     <label>Phone number</label>
                     <input value={state.phone_no} type='text' className='flex-grow'
-                    onChange={(e) => dispatch({ type: 'PHN', payload: e.target.value })} />
+                        onChange={(e) => dispatch({ type: 'PHN', payload: e.target.value })} />
                 </article>
                 <button type="submit"
-                className="submit-btn hover:bg-clr-6 hover: text-clr-5">
-                    {loading ? <SpinnerOne />: 'Submit'}
+                    className="submit-btn hover:bg-clr-6 hover: text-clr-5">
+                    {loading ? <SpinnerOne /> : 'Submit'}
                 </button>
             </form>
         </section>
