6da6979603f4780f1620458d489fb20a1516f4b7
Raheem Kawojue <kawojue08@gmail.com>
2023-07-14 21:40:12 +0100

    refractored

--- client/app/staff/profile/page.tsx
+++ client/app/staff/profile/page.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
 import { useEffect } from 'react'
-import useAuth from '@/hooks/useAuth'
 import Profile from '@/components/Profile'
 import { useRouter } from 'next/navigation'
+import { useAuthStore } from '@/utils/store'
 import { SpinnerTwo } from '@/components/Spinner'
 
 const page = () => {
     const router = useRouter()
-    const { profile, loadingProfile, auth }: any = useAuth()
+    const { auth, profile, loadProf } = useAuthStore()
 
     useEffect(() => {
         if (!auth) router.push('/staff/login')
     }, [auth, router])
-    
-    if (loadingProfile) return <SpinnerTwo />
+
+    if (loadProf) return <SpinnerTwo />
 
     return <Profile profile={profile} />
 }
