c43772de3b2e32befa4f522b5d334303247a62ff
Raheem Kawojue <kawojue08@gmail.com>
2023-07-14 21:25:02 +0100

    refractored

--- client/components/SubmitBtn.tsx
+++ client/components/SubmitBtn.tsx
@@ -1,13 +1,13 @@
 "use client"
-import useAuth from "@/hooks/useAuth"
 import { SpinnerOne } from "./Spinner"
+import { useAuthStore } from "@/utils/store"
 
 const SubmitBtn: React.FC<ISubmitBtn> = ({ loading, texts, styles, handler }) => {
-    const { loading: load } = useAuth()
+    const { loading: load } = useAuthStore()
 
     return (
         <button type="submit" onClick={async () => await handler()}
-        className={`${styles || "submit-btn hover:bg-clr-6 hover: text-clr-5"}`}>
+            className={`${styles || "submit-btn hover:bg-clr-6 hover: text-clr-5"}`}>
             {(load || loading) ? <SpinnerOne /> : <span>{texts}</span>}
         </button>
     )
