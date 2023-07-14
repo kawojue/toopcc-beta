2ff6615be3b382542e6d3af277d7b7600e9554aa
Raheem Kawojue <kawojue08@gmail.com>
2023-07-14 20:53:27 +0100

    refractored

--- client/components/Staff Modals/Resign.tsx
+++ client/components/Staff Modals/Resign.tsx
@@ -7,14 +7,15 @@ import useToken from '@/hooks/useToken'
 import { SpinnerOne } from '../Spinner'
 import throwError from '@/utils/throwError'
 import { FaTimes } from '@/public/icons/ico'
-import { FormEvent, Fragment, useState } from 'react'
+import { AxiosResponse, AxiosError } from 'axios'
 import { Dialog, Transition } from '@headlessui/react'
+import { FormEvent, Fragment, useState, FC } from 'react'
 
-const ResignModal: React.FC<IModal> = ({ state, dispatch, profile }) => {
+const ResignModal: FC<IModal> = ({ state, dispatch, profile }) => {
     const token: string = useToken()
     const [date, setDate] = useState<any>("")
-    const [loading, setLoading]= useState<boolean>(false)
-    const [resig, setResig] = useState<boolean>(profile.resigned?.resign ? true: false)
+    const [loading, setLoading] = useState<boolean>(false)
+    const [resig, setResig] = useState<boolean>(profile.resigned?.resign ? true : false)
 
     const handleResignation = async (): Promise<void> => {
         setLoading(true)
@@ -26,13 +27,13 @@ const ResignModal: React.FC<IModal> = ({ state, dispatch, profile }) => {
                     'Authorization': `Bearer ${token}`
                 }
             }
-        ).then((res: any) => {
+        ).then((res: AxiosResponse) => {
             notify(res.data?.action, "Successful.")
             dispatch({ type: "RESIG" })
             setTimeout(() => {
                 document.location.reload()
             }, 300)
-        }).catch((err: any) => throwError(err)).finally(() => setLoading(false))
+        }).catch((err: AxiosError) => throwError(err)).finally(() => setLoading(false))
     }
 
     const cancel = () => {
@@ -46,68 +47,68 @@ const ResignModal: React.FC<IModal> = ({ state, dispatch, profile }) => {
     return (
         <Transition appear show={state.resignation} as={Fragment}>
             <Dialog as="div" className="modal"
-            onClose={() => dispatch({ type: "RESIG" })}>
-            <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0">
-                <div className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
-
-            <div className="modal-main">
-                <div className="modal-center">
-                    <Transition.Child
+                onClose={() => dispatch({ type: "RESIG" })}>
+                <Transition.Child
                     as={Fragment}
                     enter="ease-out duration-300"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
                     leave="ease-in duration-200"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95">
-                        <Dialog.Panel className="modal-panel min-h-[200px]">
-                            <article className="flex items-center">
-                                <div className="w-fit">
-                                    <button className="modal-close-btn"
-                                    onClick={() => dispatch({ type: "RESIG" })}>
-                                        <FaTimes />
-                                    </button>
-                                </div>
-                                <h3 className="modal-header">Edit Resignation</h3>
-                            </article>
-                            <form className="modal-form" onSubmit={async (e: any) => {
-                                e.preventDefault();
-                                (async () => await handleResignation())()
-                            }}>
-                                <form className="modal-form" onSubmit={(e: FormEvent) => {
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0">
+                    <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
+
+                <div className="modal-main">
+                    <div className="modal-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95">
+                            <Dialog.Panel className="modal-panel min-h-[200px]">
+                                <article className="flex items-center">
+                                    <div className="w-fit">
+                                        <button className="modal-close-btn"
+                                            onClick={() => dispatch({ type: "RESIG" })}>
+                                            <FaTimes />
+                                        </button>
+                                    </div>
+                                    <h3 className="modal-header">Edit Resignation</h3>
+                                </article>
+                                <form className="modal-form" onSubmit={async (e: any) => {
                                     e.preventDefault();
                                     (async () => await handleResignation())()
                                 }}>
-                                    <SwitchBtn get={resig} set={setResig} />
-                                    <article className="modal-form-group">
-                                        <label htmlFor='date' />
-                                        <input type='date' id='date' value={date}
-                                        onChange={(e) => setDate(e.target.value)}/>
-                                    </article>
+                                    <form className="modal-form" onSubmit={(e: FormEvent) => {
+                                        e.preventDefault();
+                                        (async () => await handleResignation())()
+                                    }}>
+                                        <SwitchBtn get={resig} set={setResig} />
+                                        <article className="modal-form-group">
+                                            <label htmlFor='date' />
+                                            <input type='date' id='date' value={date}
+                                                onChange={(e) => setDate(e.target.value)} />
+                                        </article>
+                                    </form>
                                 </form>
-                            </form>
-                            <div className="modal-btn-container">
-                                <button className="save-btn" disabled={!eligible}
-                                onClick={async () => await handleResignation()}>
-                                    {loading ? <SpinnerOne /> : 'Save'}
-                                </button>
-                                <button className="cancel-btn"
-                                onClick={() => cancel()}>
-                                    Cancel
-                                </button>
-                            </div>
-                        </Dialog.Panel>
-                    </Transition.Child>
+                                <div className="modal-btn-container">
+                                    <button className="save-btn" disabled={!eligible}
+                                        onClick={async () => await handleResignation()}>
+                                        {loading ? <SpinnerOne /> : 'Save'}
+                                    </button>
+                                    <button className="cancel-btn"
+                                        onClick={() => cancel()}>
+                                        Cancel
+                                    </button>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
                 </div>
-            </div>
             </Dialog>
         </Transition>
     )
