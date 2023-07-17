6cf362675b8776925a752a49fa4915ca8b15703e
Raheem Kawojue <kawojue08@gmail.com>
2023-07-17 01:40:37 +0100

    .

--- server/dist/configs/mailConfig.js
+++ server/dist/configs/mailConfig.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
-const nodemailer_1 = __importDefault(require("nodemailer"));
+const nodemailer_1 = require("nodemailer");
 dotenv_1.default.config();
-const transporter = nodemailer_1.default.createTransport({
+const transporter = (0, nodemailer_1.createTransport)({
     host: 'smtp.gmail.com',
     secure: true,
     service: 'gmail',
