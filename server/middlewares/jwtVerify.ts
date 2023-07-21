49b186d37ca1758e8ce91845a7fa2088ef1b7025
Raheem Kawojue <kawojue08@gmail.com>
2023-07-21 23:07:38 +0100

    integrate prisma

--- server/middlewares/jwtVerify.ts
+++ server/middlewares/jwtVerify.ts
@@ -1,22 +1,26 @@
 import jwt from 'jsonwebtoken'
-import User from '../models/User'
+import prisma from '../prisma'
+import { IRequest } from '../type'
 import { Response, NextFunction } from 'express'
 import { ACCESS_DENIED } from '../utilities/modal'
-const asyncHandler = require('express-async-handler')
+const expressAsyncHandler = require('express-async-handler')
 
-const jwtVerify = asyncHandler(async (req: any, res: Response, next: NextFunction) => {
+const jwtVerify = expressAsyncHandler(async (req: IRequest, res: Response, next: NextFunction) => {
     const authHeader = req.headers?.authorization
     if (!authHeader || !authHeader?.startsWith('Bearer')) return res.status(401).json(ACCESS_DENIED)
 
-    const token: any = authHeader?.split(' ')[1]
+    const token: string = authHeader?.split(' ')[1]
     jwt.verify(
         token,
         process.env.JWT_SECRET as string,
         async (err: any, decoded: any) => {
             if (err) return res.status(403).json(ACCESS_DENIED)
 
-            const account: any = await User.findOne({ token })
-            .select('-password -OTP').exec()
+            const account = await prisma.user.findUnique({
+                where: {
+                    user: decoded.user
+                }
+            })
             if (!account) return res.status(403).json(ACCESS_DENIED)
 
             const user: string = decoded.user
@@ -24,11 +28,17 @@ const jwtVerify = asyncHandler(async (req: any, res: Response, next: NextFunctio
             const authRoles: string[] = account.roles
 
             if (account.resigned.resign || roles.length !== authRoles.length) {
-                account.token = ""
-                await account.save()
+                await prisma.user.update({
+                    where: {
+                        user: decoded.user
+                    },
+                    data: {
+                        token: ""
+                    }
+                })
                 return res.status(403).json(ACCESS_DENIED)
             }
-            
+
             req.user = user
             req.roles = roles
             next()
