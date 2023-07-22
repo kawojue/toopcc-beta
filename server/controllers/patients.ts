d8c438cfe8d5dd5943dfdc916ba38c3c8844820a
Raheem Kawojue <kawojue08@gmail.com>
2023-07-23 00:33:29 +0100

    integrate prisma

--- server/controllers/patients.ts
+++ server/controllers/patients.ts
@@ -1,3 +1,4 @@
+import prisma from '../prisma'
 import { v4 as uuid } from 'uuid'
 import { IBody, ICloud } from '../type'
 import Patient from '../models/Patient'
@@ -6,8 +7,8 @@ import { Request, Response } from 'express'
 import addMedic from '../utilities/addMedic'
 import full_name from '../utilities/full_name'
 import cloudinary from '../configs/cloudinary'
+import StatusCodes from '../utilities/StatusCodes'
 import addExtension from '../utilities/addExtension'
-import { fetchByCardNumber } from '../utilities/getModels'
 import {
     PATIENT_NOT_EXIST, SMTH_WENT_WRONG, PATIENT_EXIST, SAVED, SUCCESS,
     DELETION_FAILED, EXT_NOT_EXIST, DIAG_NOT_EXIST, INVALID_PHONE_NO,
@@ -24,41 +25,56 @@ const add = asyncHandler(async (req: Request, res: Response) => {
         address, age, date, sex
     }: any = req.body
 
-    card_no = card_no?.trim()?.toUpperCase()
     address = address?.trim()
-    fullname = fullname?.trim()
-
-    if (!card_no || !fullname || !sex || !age) return res.status(400).json(FIELDS_REQUIRED)
-
     fullname = full_name(fullname)
+    card_no = card_no?.trim()?.toUpperCase()
+
+    if (!card_no || !fullname || !sex || !age) {
+        return res.status(StatusCodes.BadRequest).json(FIELDS_REQUIRED)
+    }
+
     if (/^\d/.test(age)) {
         age = Number(age)
     }
 
     if (!/^[a-zA-Z0-9]+$/.test(card_no)) {
-        return res.status(400).json({
+        return res.status(StatusCodes.BadRequest).json({
             ...ERROR,
             msg: "Invalid card number."
         })
     }
 
     if (phone_no?.trim()) {
-        if (!phoneRegex.test(phone_no?.trim())) return res.status(400).json(INVALID_PHONE_NO)
+        if (!phoneRegex.test(phone_no?.trim())) {
+            return res.status(StatusCodes.BadRequest).json(INVALID_PHONE_NO)
+        }
     }
 
-    if (age > 120) return res.status(400).json(INVALID_AGE)
-    if (date) date = date
+    if (age > 120) {
+        return res.status(StatusCodes.BadRequest).json(INVALID_AGE)
+    }
 
-    const patient: any = await Patient.findOne({ card_no }).exec()
-    if (patient) return res.status(409).json(PATIENT_EXIST)
+    if (date) {
+        date = date
+    }
 
-    await Patient.create({
-        sex, card_no, date,
-        address, fullname,
-        phone_no, age: age,
+    const patient = await prisma.patient.findUnique({
+        where: { card_no }
     })
 
-    res.status(201).json(SAVED)
+    if (patient) {
+        return res.status(StatusCodes.Conflict).json(PATIENT_EXIST)
+    }
+
+    await prisma.patient.create({
+        data: {
+            sex, card_no, date,
+            address, fullname,
+            phone_no, age: age,
+        }
+    })
+
+    res.status(StatusCodes.Created).json(SAVED)
 })
 
 // edit patient data
@@ -70,19 +86,19 @@ const edit = asyncHandler(async (req: Request, res: Response) => {
     }: any = req.body
 
     const patient: any = await fetchByCardNumber(card_no, '-body -recommendation')
-    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+    if (!patient) return res.status(StatusCodes.NotFound).json(PATIENT_NOT_EXIST)
 
     if (cardNo || cardNo?.trim()) {
         cardNo = cardNo.trim().toUpperCase()
         const cardNoExists: any = await fetchByCardNumber(cardNo, '-body -recommendation')
-        if (cardNoExists) return res.status(409).json(PATIENT_EXIST)
+        if (cardNoExists) return res.status(StatusCodes.Conflict).json(PATIENT_EXIST)
         patient.card_no = cardNo
     }
 
     if (fullname) {
         fullname = full_name(fullname)
         if (!fullname) {
-            return res.status(400).json({
+            return res.status(StatusCodes.BadRequest).json({
                 ...ERROR,
                 msg: "Fullname is required."
             })
@@ -92,7 +108,7 @@ const edit = asyncHandler(async (req: Request, res: Response) => {
 
     if (sex) {
         if (sex !== "Male" && sex !== "Female") {
-            return res.status(400).json({
+            return res.status(StatusCodes.BadRequest).json({
                 ...ERROR,
                 msg: "Sex is required."
             })
@@ -102,7 +118,7 @@ const edit = asyncHandler(async (req: Request, res: Response) => {
 
     if (phone_no) {
         if (!phone_no?.trim() || !phoneRegex.test(phone_no?.trim())) {
-            return res.status(400).json({
+            return res.status(StatusCodes.BadRequest).json({
                 ...ERROR,
                 msg: "Invalid phone number"
             })
@@ -113,7 +129,7 @@ const edit = asyncHandler(async (req: Request, res: Response) => {
     if (age) {
         age = Number(age)
         if (age > 120) {
-            return res.status(400).json({
+            return res.status(StatusCodes.BadRequest).json({
                 ...ERROR,
                 msg: "Age is not valid"
             })
@@ -124,7 +140,7 @@ const edit = asyncHandler(async (req: Request, res: Response) => {
     if (death) {
         console.log(death)
         if (death.dead === true && !death.date) {
-            return res.status(400).json({
+            return res.status(StatusCodes.BadRequest).json({
                 ...ERROR,
                 msg: 'Death date is required'
             })
@@ -142,24 +158,24 @@ const edit = asyncHandler(async (req: Request, res: Response) => {
 
     await patient.save()
 
-    res.status(200).json(SAVED)
+    res.status(StatusCodes.OK).json(SAVED)
 })
 
 // delete patient data
 const remove = asyncHandler(async (req: Request, res: Response) => {
     let { card_no }: any = req.params
-    if (!card_no || !card_no?.trim()) return res.status(400).json(CARD_NO_REQUIRED)
+    if (!card_no || !card_no?.trim()) return res.status(StatusCodes.BadRequest).json(CARD_NO_REQUIRED)
 
     const patient: any = await fetchByCardNumber(card_no)
-    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+    if (!patient) return res.status(StatusCodes.NotFound).json(PATIENT_NOT_EXIST)
 
     const bodies: any[] = patient.body
     const del: boolean = await delDiag(bodies)
-    if (!del) return res.status(400).json(DELETION_FAILED)
+    if (!del) return res.status(StatusCodes.BadRequest).json(DELETION_FAILED)
 
     await Patient.deleteOne({ card_no }).exec()
 
-    res.status(200).json({
+    res.status(StatusCodes.OK).json({
         ...SUCCESS,
         msg: "Patient data has been deleted."
     })
@@ -174,20 +190,20 @@ const addDiagnosis = asyncHandler(async (req: Request, res: Response) => {
     }: any = req.body
 
     const patient = await fetchByCardNumber(card_no, '-recommendation')
-    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+    if (!patient) return res.status(StatusCodes.NotFound).json(PATIENT_NOT_EXIST)
 
-    if (!date) return res.status(400).json({ ...ERROR, msg: "Current date is required." })
+    if (!date) return res.status(StatusCodes.BadRequest).json({ ...ERROR, msg: "Current date is required." })
     if (texts || texts?.trim()) texts = texts.trim()
 
     if (images.length > 0) {
-        if (images.length > 3) return res.status(400).json(SMTH_WENT_WRONG)
+        if (images.length > 3) return res.status(StatusCodes.BadRequest).json(SMTH_WENT_WRONG)
 
         images.forEach(async (image: any) => {
             imageRes = await cloudinary.uploader.upload(image, {
                 folder: `TOOPCC/${patient.id}`,
                 resource_type: 'image'
             })
-            if (!imageRes) return res.status(400).json(SMTH_WENT_WRONG)
+            if (!imageRes) return res.status(StatusCodes.BadRequest).json(SMTH_WENT_WRONG)
 
             imageArr.push({
                 secure_url: imageRes.secure_url,
@@ -210,7 +226,7 @@ const addDiagnosis = asyncHandler(async (req: Request, res: Response) => {
     ] : [...patient.body]
     await patient.save()
 
-    res.status(200).json(SAVED)
+    res.status(StatusCodes.OK).json(SAVED)
 })
 
 const editDiagnosis = asyncHandler(async (req: Request, res: Response) => {
@@ -220,10 +236,10 @@ const editDiagnosis = asyncHandler(async (req: Request, res: Response) => {
     }: any = req.body
 
     const patient: any = await fetchByCardNumber(card_no, '-recommendation')
-    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+    if (!patient) return res.status(StatusCodes.NotFound).json(PATIENT_NOT_EXIST)
 
     const body: any = patient.body.find((body: IBody) => body.idx === idx)
-    if (!body) return res.status(404).json(DIAG_NOT_EXIST)
+    if (!body) return res.status(StatusCodes.NotFound).json(DIAG_NOT_EXIST)
 
     if (texts) body.diagnosis.texts = texts.trim()
     if (date) body.date = date
@@ -231,7 +247,7 @@ const editDiagnosis = asyncHandler(async (req: Request, res: Response) => {
 
     await patient.save()
 
-    res.status(200).json(SAVED)
+    res.status(StatusCodes.OK).json(SAVED)
 })
 
 const addRecommendation = asyncHandler(async (req: Request, res: Response) => {
@@ -242,7 +258,7 @@ const addRecommendation = asyncHandler(async (req: Request, res: Response) => {
     }: any = req.body
 
     const patient: any = await fetchByCardNumber(card_no, '-body')
-    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+    if (!patient) return res.status(StatusCodes.NotFound).json(PATIENT_NOT_EXIST)
 
     const rec: any = patient.recommendation
     const opthalmology: any = rec.opthalmology
@@ -271,14 +287,14 @@ const addRecommendation = asyncHandler(async (req: Request, res: Response) => {
 
     await patient.save()
 
-    res.status(200).json(SAVED)
+    res.status(StatusCodes.OK).json(SAVED)
 })
 
 const deleteRecommendation = asyncHandler(async (req: Request, res: Response) => {
     const { type }: any = req.query
     const { card_no, idx }: any = req.params
     const patient: any = await fetchByCardNumber(card_no, '-body')
-    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+    if (!patient) return res.status(StatusCodes.NotFound).json(PATIENT_NOT_EXIST)
 
     const rec: any = patient.recommendation
     const originalRec: any[] = type === "opthal" ?
@@ -294,63 +310,63 @@ const deleteRecommendation = asyncHandler(async (req: Request, res: Response) =>
         type === "physio" ? rec.physiotherapy.medication = newRec : null
     await patient.save()
 
-    res.status(200).json(SAVED)
+    res.status(StatusCodes.OK).json(SAVED)
 })
 
 const deletExtension = asyncHandler(async (req: Request, res: Response) => {
     const { card_no, idx }: any = req.params
     const patient: any = await fetchByCardNumber(card_no, '-body')
-    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+    if (!patient) return res.status(StatusCodes.NotFound).json(PATIENT_NOT_EXIST)
 
     const extensions: any[] = patient.recommendation.extensions
     const ext: any = extensions.find((element: any) => element.idx === idx)
-    if (!ext) return res.status(404).json(EXT_NOT_EXIST)
+    if (!ext) return res.status(StatusCodes.NotFound).json(EXT_NOT_EXIST)
 
     patient.recommendation.extensions = extensions.filter((element: any) => element.idx !== idx)
     await patient.save()
 
-    res.status(200).json(SAVED)
+    res.status(StatusCodes.OK).json(SAVED)
 })
 
 const editExtension = asyncHandler(async (req: Request, res: Response) => {
     const { extension }: any = req.body
     const { card_no, idx }: any = req.params
     const patient: any = await fetchByCardNumber(card_no, '-body')
-    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+    if (!patient) return res.status(StatusCodes.NotFound).json(PATIENT_NOT_EXIST)
 
     const extensions: any[] = patient.recommendation.extensions
     const extt: any = extensions.find((element: any) => element.idx === idx)
-    if (!extt) return res.status(404).json(EXT_NOT_EXIST)
+    if (!extt) return res.status(StatusCodes.NotFound).json(EXT_NOT_EXIST)
 
     patient.recommendation.extensions = extensions.map((ext: any) => ext.idx === idx ? {
         ...ext, ...extension
     } : ext)
     await patient.save()
 
-    res.status(200).json(SAVED)
+    res.status(StatusCodes.OK).json(SAVED)
 })
 
 const deleteDianosis = asyncHandler(async (req: Request, res: Response) => {
     const { card_no, idx }: any = req.params
 
     const patient: any = await fetchByCardNumber(card_no, '-recommendation')
-    if (!patient) return res.status(404).json(PATIENT_NOT_EXIST)
+    if (!patient) return res.status(StatusCodes.NotFound).json(PATIENT_NOT_EXIST)
 
     const bodies: IBody[] = patient.body
     const body: any = bodies.find((body: IBody) => body.idx === idx)
-    if (!body) return res.status(404).json(DIAG_NOT_EXIST)
+    if (!body) return res.status(StatusCodes.NotFound).json(DIAG_NOT_EXIST)
 
     const images: ICloud[] = body.diagnosis.images
     if (images.length > 0) {
         images.forEach(async (image: ICloud) => {
             const result: any = await cloudinary.uploader.destroy(image.public_id)
-            if (!result) return res.status(400).json(DELETION_FAILED)
+            if (!result) return res.status(StatusCodes.BadRequest).json(DELETION_FAILED)
         })
     }
     patient.body = bodies.filter((body: IBody) => body.idx !== idx)
     await patient.save()
 
-    res.status(200).json({
+    res.status(StatusCodes.OK).json({
         ...SUCCESS,
         msg: "Deleted successfully"
     })
