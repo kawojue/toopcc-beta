b1c81cb91b70c0727238f225fc04e6ce713385c9
Raheem Kawojue <kawojue08@gmail.com>
2023-07-23 00:07:19 +0100

    refractored

--- server/utilities/full_name.ts
+++ server/utilities/full_name.ts
@@ -1,6 +1,8 @@
 export default function full_name(fullname: string): string {
+    if (!fullname?.trim()) return ""
+
     let actualName: string = ""
-    const names: any[] = fullname?.split(" ") || []
+    const names: any[] = fullname.split(" ") || []
     names.forEach((name: string) => {
         actualName += name[0].toUpperCase() + name.slice(1).toLowerCase() + " "
     })
