d50170b31a0a02c7e79ff042528b08c151e6685e
Raheem Kawojue <kawojue08@gmail.com>
2023-07-24 00:19:34 +0100

    integrate prisma

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,29 +1,58 @@
-import {
-    fetchUsers, fetchUserByUser
-} from '../utilities/getModels'
+import prisma from '../prisma'
+import { IRequest } from '../type'
 import { Response } from 'express'
 import { SUCCESS } from '../utilities/modal'
-import expressAsyncHandler from 'express-async-handler'
+const expressAsyncHandler = require('express-async-handler')
 
-const getProfile = expressAsyncHandler(async (req: any, res: Response) => {
-    const profile: any = await fetchUserByUser(req?.user, '-token -password -OTP')
-    
+const getProfile = expressAsyncHandler(async (req: IRequest, res: Response) => {
+    const profile = await prisma.user.findUnique({
+        where: {
+            user: req.user
+        },
+        select: {
+            id: true,
+            user: true,
+            mail: true,
+            avatar: true,
+            fullname: true,
+            resigned: true,
+            createdAt: true,
+            lastLogin: true
+        }
+    })
+
     res.status(200).json({ ...SUCCESS, profile })
 })
 
-const getUsers = expressAsyncHandler(async (req: any, res: Response) => {
-    const users: any = await fetchUsers()
-    const names: string[] = users.map((user: any) => {
+const getUsers = expressAsyncHandler(async (req: IRequest, res: Response) => {
+    const users = await prisma.user.findMany()
+
+    const names = users.map((user) => {
         return { username: user.user, fullname: user.fullname }
     })
 
     res.status(200).json({ ...SUCCESS, names })
 })
 
-const getUser = expressAsyncHandler(async (req: any, res: Response) => {
+const getUser = expressAsyncHandler(async (req: IRequest, res: Response) => {
     const { user: userParam }: any = req.params
-    const user: any = await fetchUserByUser(userParam, '-token -password -OTP')
-    
+
+    const user: any = await prisma.user.findUnique({
+        where: {
+            user: userParam
+        },
+        select: {
+            id: true,
+            user: true,
+            mail: true,
+            avatar: true,
+            fullname: true,
+            resigned: true,
+            createdAt: true,
+            lastLogin: true
+        }
+    })
+
     res.status(200).json({ ...SUCCESS, user })
 })
 
