0c727f0607e8684959785efba76ebf5a3b360828
Raheem Kawojue <kawojue08@gmail.com>
2023-07-28 14:07:58 +0100

    .

--- server/dist/index.js
+++ server/dist/index.js
@@ -8,8 +8,6 @@ dotenv_1.default.config();
 // import utilities
 const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
-const mongoose_1 = __importDefault(require("mongoose"));
-const dbConn_1 = __importDefault(require("./configs/dbConn"));
 const corsOption_1 = __importDefault(require("./configs/corsOption"));
 const express_1 = __importDefault(require("express"));
 const credentials_1 = __importDefault(require("./middlewares/credentials"));
@@ -17,7 +15,6 @@ const credentials_1 = __importDefault(require("./middlewares/credentials"));
 const root_1 = __importDefault(require("./routes/root"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 1002;
-(0, dbConn_1.default)(process.env.DB_URI);
 // set middlewares
 app.use(credentials_1.default);
 app.use(express_1.default.json({ limit: '14mb' }));
@@ -27,6 +24,4 @@ app.use(express_1.default.urlencoded({
     limit: '14mb', extended: true
 }));
 app.use('/', root_1.default);
-mongoose_1.default.connection.once('open', () => {
-    app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-});
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
