f334223b525c082e1959cdcae1f4b11912b627da
Raheem Kawojue <kawojue08@gmail.com>
2023-07-28 13:21:36 +0100

    refactored

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,33 +1,17 @@
-import prisma from '../prisma'
 import { IRequest } from '../type'
 import { Response } from 'express'
 import { SUCCESS } from '../utilities/modal'
+import { findByUser, fetchUsers } from '../utilities/model'
 const expressAsyncHandler = require('express-async-handler')
 
-const selected = {
-    id: true,
-    user: true,
-    mail: true,
-    avatar: true,
-    fullname: true,
-    resigned: true,
-    createdAt: true,
-    lastLogin: true
-}
-
 const getProfile = expressAsyncHandler(async (req: IRequest, res: Response) => {
-    const profile = await prisma.user.findUnique({
-        where: {
-            user: req.user
-        },
-        select: selected
-    })
+    const profile = await fetchUsers('-totp -email_verified -totp_date -token -password')
 
     res.status(200).json({ ...SUCCESS, profile })
 })
 
 const getUsers = expressAsyncHandler(async (req: IRequest, res: Response) => {
-    const users = await prisma.user.findMany()
+    const users = await fetchUsers('-totp -email_verified -totp_date -token -password')
 
     const names = users.map((user) => {
         return { username: user.user, fullname: user.fullname }
@@ -39,12 +23,7 @@ const getUsers = expressAsyncHandler(async (req: IRequest, res: Response) => {
 const getUser = expressAsyncHandler(async (req: IRequest, res: Response) => {
     const { user: userParam }: any = req.params
 
-    const user: any = await prisma.user.findUnique({
-        where: {
-            user: userParam
-        },
-        select: selected
-    })
+    const user: any = await findByUser(userParam, '-totp -email_verified -totp_date -token -password')
 
     res.status(200).json({ ...SUCCESS, user })
 })
