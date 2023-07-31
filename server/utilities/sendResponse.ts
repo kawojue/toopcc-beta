1775d78a5a9de4384d2ea58fedde9f83267c8652
Raheem Kawojue <kawojue08@gmail.com>
2023-07-31 15:02:45 +0100

    refactored

--- server/utilities/sendResponse.ts
+++ server/utilities/sendResponse.ts
@@ -2,12 +2,10 @@ import { Response } from 'express'
 
 const sendError = (res: Response, statusCode: number, msg: string) => {
     res.status(statusCode).json({ success: false, msg })
-    return
 }
 
 const sendSuccess = (res: Response, statusCode: number, data?: any) => {
     res.status(statusCode).json({ success: true, ...data })
-    return
 }
 
 export { sendError, sendSuccess }
