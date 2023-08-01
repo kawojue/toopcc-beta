21686924242594355592513a50b31b7fc8b82e23
Raheem Kawojue <kawojue08@gmail.com>
2023-08-01 21:48:38 +0100

    ..

--- client/utils/genNextApp.ts
+++ client/utils/genNextApp.ts
@@ -1,6 +1,5 @@
-
 export default function getNextAppDates(lastVisitDate: string) {
-    const maxFutureMonths: number = 5
+    const maxFutureMonths: number = 4
     const nextAppointmentDates: string[] = []
 
     const endDate: Date = new Date()
@@ -8,8 +7,8 @@ export default function getNextAppDates(lastVisitDate: string) {
     endDate.setMonth(endDate.getMonth() + maxFutureMonths)
 
     for (let currentDate: Date = new Date(startDate);
-    currentDate <= endDate;
-    currentDate.setDate(currentDate.getDate() + 1)) {
+        currentDate <= endDate;
+        currentDate.setDate(currentDate.getDate() + 1)) {
         const currentDay = currentDate.getDay()
         if (currentDay === 2 || currentDay === 4) {
             nextAppointmentDates.push(currentDate.toISOString())
