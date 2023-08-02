a154852251cc442f5eb3c66da1bc9ef893d8e1e0
Raheem Muyiwa <kawojue08@gmail.com>
2023-08-02 08:43:41 +0100

    Update instance.ts

--- client/app/api/instance.ts
+++ client/app/api/instance.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const BASE_URL: unknown = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_SERVER_URL:  'http://localhost:1002'
 
 export default axios.create({
-    baseURL: BASE_URL as string,
+    baseURL: BASE_URL!,
     headers: {
         'Content-Type': 'application/json',
     },
