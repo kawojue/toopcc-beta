e76731b160dcdcbf01e1c988e025e86e7254bd38
Raheem Kawojue <kawojue08@gmail.com>
2023-08-04 08:02:00 +0100

    refactored

--- server/routes/auth/role.ts
+++ server/routes/auth/role.ts
@@ -1,16 +1,15 @@
 import { Router } from 'express'
+import {
+    resigned, editRoles
+} from '../../controllers/userAuth'
 import jwtVerify from '../../middlewares/jwtVerify'
 import verifyRoles from '../../middlewares/verifyRoles'
-import {
-    resigned, changeRoles, removeRole
-} from '../../controllers/userAuth'
 
 const role: Router = Router()
 
 role.use([jwtVerify, verifyRoles("hr")])
 
+role.put('/role', editRoles)
 role.post('/resign/:user', resigned)
-role.post('/assign/:user', changeRoles)
-role.post('/remove/:user', removeRole)
 
 export default role
