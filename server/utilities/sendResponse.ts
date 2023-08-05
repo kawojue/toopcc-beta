da28347c9addf91db21f7672c5cc11c9174ae396
Raheem Muyiwa <kawojue08@gmail.com>
2023-08-06 00:47:17 +0100

    Update sendResponse.ts

--- server/utilities/sendResponse.ts
+++ server/utilities/sendResponse.ts
@@ -1,10 +1,10 @@
 import { Response } from 'express'
 
-const sendError = (res: Response, statusCode: number, msg: string) => {
+const sendError = (res: Response, statusCode: number, msg: string): void => {
     res.status(statusCode).json({ success: false, msg })
 }
 
-const sendSuccess = (res: Response, statusCode: number, data?: any) => {
+const sendSuccess = (res: Response, statusCode: number, data?: any): void => {
     res.status(statusCode).json({ success: true, ...data })
 }
 
