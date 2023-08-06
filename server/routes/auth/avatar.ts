9194fa862b8c9b9737ece598d0ce299097014ef7
Raheem Kawojue <kawojue08@gmail.com>
2023-08-06 19:00:18 +0100

    [set] upload middleware

--- server/routes/auth/avatar.ts
+++ server/routes/auth/avatar.ts
@@ -2,15 +2,12 @@ import { Router } from 'express'
 import {
     changeAvatar, deleteAvatar
 } from '../../controllers/userAuth'
+import upload from '../../middlewares/upload'
 import jwtVerify from '../../middlewares/jwtVerify'
-import multer, { StorageEngine, Multer } from 'multer'
 
 const avatar: Router = Router()
 
 avatar.use(jwtVerify)
-
-const storage: StorageEngine = multer.memoryStorage()
-const upload: Multer = multer({ storage })
 
 avatar.route('/')
     .post(upload.single('avatar'), changeAvatar)
