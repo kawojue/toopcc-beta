ed45d7d211e779beccc0bb94db91c7d806f097e7
Raheem Kawojue <kawojue08@gmail.com>
2023-08-07 02:47:56 +0100

    [add] getS3

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -1,18 +1,27 @@
 import { IRequest } from '../type'
 import { Response } from 'express'
+import { getS3 } from '../utilities/s3'
 import StatusCodes from '../utilities/StatusCodes'
 import { sendSuccess } from '../utilities/sendResponse'
 import { findByUser, fetchUsers } from '../utilities/model'
 const expressAsyncHandler = require('express-async-handler')
 
+
+const omit: string = '-totp -email_verified -totp_date -token -password'
+
 const getProfile = expressAsyncHandler(async (req: IRequest, res: Response) => {
-    const profile = await findByUser(req.user, '-totp -email_verified -totp_date -token -password')
+    const getProfile = await findByUser(req.user, omit)
+
+    const profile = {
+        ...getProfile,
+        avatarUrl: getProfile?.avatar_path ? await getS3(getProfile?.avatar_path) : ""
+    }
 
     sendSuccess(res, StatusCodes.OK, { profile })
 })
 
 const getUsers = expressAsyncHandler(async (req: IRequest, res: Response) => {
-    const users = await fetchUsers('-totp -email_verified -totp_date -token -password')
+    const users = await fetchUsers(omit)
 
     const names = users.map((user) => {
         return { username: user.user, fullname: user.fullname }
@@ -24,7 +33,11 @@ const getUsers = expressAsyncHandler(async (req: IRequest, res: Response) => {
 const getUser = expressAsyncHandler(async (req: IRequest, res: Response) => {
     const { user: userParam }: any = req.params
 
-    const user: any = await findByUser(userParam, '-totp -email_verified -totp_date -token -password')
+    const getUser: any = await findByUser(userParam, omit)
+    const user = {
+        ...getUser,
+        avatarUrl: getUser?.avatar_path ? await getS3(getUser?.avatar_path) : ""
+    }
 
     sendSuccess(res, StatusCodes.OK, { user })
 })
