30e3065c6fd19e3121233f071aae72fbf2254b8b
Raheem Kawojue <kawojue08@gmail.com>
2023-08-08 23:06:58 +0100

    [fix] error

--- server/middlewares/verifyRoles.ts
+++ server/middlewares/verifyRoles.ts
@@ -1,14 +1,22 @@
 import checkRoles from '../utilities/checkRoles'
 import { Response, NextFunction } from 'express'
 import { ACCESS_DENIED } from '../utilities/modal'
+import { sendError } from '../utilities/sendResponse'
+import StatusCodes from '../utilities/StatusCodes'
 
 const verifyRoles = (...roles: string[]) => {
     return (req: any, res: Response, next: NextFunction) => {
-        if (!req?.roles) return res.status(401).json(ACCESS_DENIED)
+        if (!req?.roles) {
+            sendError(res, StatusCodes.Unauthorized, ACCESS_DENIED)
+            return
+        }
 
         const authRoles: string[] = req.roles
         const allowedRoles: string[] = [...roles]
-        if (!checkRoles(authRoles, allowedRoles)) return res.status(401).json(ACCESS_DENIED)
+        if (!checkRoles(authRoles, allowedRoles)) {
+            sendError(res, StatusCodes.Unauthorized, ACCESS_DENIED)
+            return
+        }
 
         next()
     }
