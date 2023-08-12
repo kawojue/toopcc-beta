fcaf65884779d304e6d3162d44874dbf29d76ed0
Raheem Kawojue <kawojue08@gmail.com>
2023-08-12 01:08:08 +0100

    .

--- server/middlewares/limiter.ts
+++ server/middlewares/limiter.ts
@@ -9,7 +9,7 @@ export default function limiterFunc({
 }: ILimiter): RateLimitRequestHandler {
     const limiter: RateLimitRequestHandler = rateLimit({
         max, // max attempt
-        windowMs: timerArr[Math.floor(Math.random() * timerArr.length)] * 1000, // try again in
+        windowMs: timerArr[Math.floor(Math.random() * timerArr.length)] * 1000, // throttle
         message: { msg },
         handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
             sendError(res, options.statusCode, options.message?.msg)
